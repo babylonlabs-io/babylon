942e421717f9728d45fd207daa49911326b85958
Mauro Lacy <maurolacy@users.noreply.github.com>
2024-03-15 08:29:59 +0100

    F/register codecs (#210)
    * Register zoneconcierge msg codecs
    * Register btcstkconsumer msg codecs

--- x/btcstkconsumer/types/codec.go
+++ x/btcstkconsumer/types/codec.go
@@ -9,11 +9,19 @@ import (
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgUpdateParams{}, "btcstkconsumer/MsgUpdateParams", nil)
+	cdc.RegisterConcrete(&MsgRegisterChain{}, "btcstkconsumer/MsgRegisterChain", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgUpdateParams{},
+		&MsgRegisterChain{},
 	)
+
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
+
+var (
+	Amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+)
