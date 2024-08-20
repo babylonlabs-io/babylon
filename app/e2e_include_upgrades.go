51486e5ef71d1b005842615a8cd70a75c1d392b4
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-08-19 23:20:53 -0300

    chore: add comment for init to be removed

--- app/e2e_include_upgrades.go
+++ app/e2e_include_upgrades.go
@@ -6,6 +6,8 @@ import (
 	"github.com/babylonlabs-io/babylon/app/upgrades/signetlaunch"
 )
 
+// init is used to include signet upgrade used for e2e testing
+// this file should be removed once the upgrade testing with signet ends.
 func init() {
 	Upgrades = append(Upgrades, signetlaunch.Upgrade)
 }
