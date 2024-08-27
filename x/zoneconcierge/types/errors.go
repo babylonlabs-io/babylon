3d713f20b76d58274ba55edca22450f8887e6215
Runchao Han <me@runchao.rocks>
2024-08-28 08:26:32 +1000

    zoneconcierge: replace chain ID with client ID for consumers (#32)
    Subsumes #22 
    
    ---
    
    Fixes https://github.com/babylonlabs-io/pm/issues/12
    
    This PR replaces the chain ID with the client ID for consumers in the
    BTC timestamping protocol. This is in line with [the IBC security
    model](https://tutorials.cosmos.network/academy/3-ibc/1-what-is-ibc.html)
    where each IBC light client corresponds to a chain, and with the BTC
    staking integration implementation.
    
    The PR includes:
    
    - renaming all chain ID to consumer ID, same as BTC staking integration
    does
    - using client ID to identify {epoch, finalised} chain info
    - changing all queries, fuzz tests, e2e tests, and doc accordingly
    
    TODO before merging the PR:
    
    - [x] wait until @gitferry verifies we can do software upgrade with
    empty DB and updated DB schema
    
    Plan after the PR:
    
    - Fixing compatiblity with BE/FE (need arch decisions here)

--- x/zoneconcierge/types/errors.go
+++ x/zoneconcierge/types/errors.go
@@ -15,5 +15,5 @@ var (
 	ErrInvalidProofEpochSealed = errorsmod.Register(ModuleName, 1107, "invalid ProofEpochSealed")
 	ErrInvalidMerkleProof      = errorsmod.Register(ModuleName, 1108, "invalid Merkle inclusion proof")
 	ErrInvalidChainInfo        = errorsmod.Register(ModuleName, 1109, "invalid chain info")
-	ErrInvalidChainIDs         = errorsmod.Register(ModuleName, 1110, "chain ids contain duplicates or empty strings")
+	ErrInvalidConsumerIDs      = errorsmod.Register(ModuleName, 1110, "chain ids contain duplicates or empty strings")
 )
