13b1e35d37ea3e3346ae87e721bb9a1df2bdb0bc
Cirrus Gai <greferry@gmail.com>
2024-09-30 16:07:26 +0800

    chore: Add signing info query cmd (#106)

--- x/finality/client/cli/query.go
+++ x/finality/client/cli/query.go
@@ -35,6 +35,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdListBlocks())
 	cmd.AddCommand(CmdVotesAtHeight())
 	cmd.AddCommand(CmdListEvidences())
+	cmd.AddCommand(CmdSigningInfo())
+	cmd.AddCommand(CmdAllSigningInfo())
 
 	return cmd
 }
@@ -241,6 +243,70 @@ func CmdListEvidences() *cobra.Command {
 	flags.AddQueryFlagsToCmd(cmd)
 	flags.AddPaginationFlagsToCmd(cmd, "list-evidences")
 	cmd.Flags().Uint64(flagStartHeight, 0, "Starting height for scanning evidences")
+
+	return cmd
+}
+
+func CmdSigningInfo() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "signing-info [fp-pk-hex]",
+		Short: "Show signing info of a given finality provider",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			fpPkHex := args[0]
+
+			// query for the signing info of a given finality provider
+			res, err := queryClient.SigningInfo(
+				cmd.Context(),
+				&types.QuerySigningInfoRequest{FpBtcPkHex: fpPkHex},
+			)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdAllSigningInfo() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "all-signing-info",
+		Short: "Show signing info of finality providers",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			// query for all the signing infos
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			res, err := queryClient.SigningInfos(
+				cmd.Context(),
+				&types.QuerySigningInfosRequest{Pagination: pageReq},
+			)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "all-signing-info")
 
 	return cmd
 }
