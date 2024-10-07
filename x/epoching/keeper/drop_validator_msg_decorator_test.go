b128f1d0f7b26982bd78bd7b998ae61ff5f9ca48
Runchao Han <me@runchao.rocks>
2024-10-07 18:15:17 +1100

    fix: intercept staking module messages inside MsgExec (#138)
    Resolves #127
    
    This PR fixes the `DropValidatorMsgDecorator` AnteHandler to handle
    disallowed staking related messages inside `MsgExec` (ref
    https://jumpcrypto.com/writing/bypassing-ethermint-ante-handlers/).

--- x/epoching/keeper/drop_validator_msg_decorator_test.go
+++ x/epoching/keeper/drop_validator_msg_decorator_test.go
@@ -3,35 +3,55 @@ package keeper
 import (
 	"testing"
 
+	"github.com/babylonlabs-io/babylon/x/epoching/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authz "github.com/cosmos/cosmos-sdk/x/authz"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/stretchr/testify/require"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
+
+func newMsgExecWithStakingMsg() *authz.MsgExec {
+	msg := authz.NewMsgExec(sdk.AccAddress("test"), []sdk.Msg{
+		&stakingtypes.MsgCreateValidator{},
+		&stakingtypes.MsgEditValidator{},
+	})
+	return &msg
+}
+
+func newValidMsgExec() *authz.MsgExec {
+	msg := authz.NewMsgExec(sdk.AccAddress("test"), []sdk.Msg{
+		&stakingtypes.MsgEditValidator{},
+	})
+	return &msg
+}
 
 func TestDropValidatorMsgDecorator(t *testing.T) {
 	testCases := []struct {
-		msg        sdk.Msg
-		expectPass bool
+		msg       sdk.Msg
+		expectErr error
 	}{
 		// wrapped message types that should be rejected
-		{&stakingtypes.MsgCreateValidator{}, true},
-		{&stakingtypes.MsgDelegate{}, true},
-		{&stakingtypes.MsgUndelegate{}, true},
-		{&stakingtypes.MsgBeginRedelegate{}, true},
-		{&stakingtypes.MsgCancelUnbondingDelegation{}, true},
+		{&stakingtypes.MsgCreateValidator{}, types.ErrUnwrappedMsgType},
+		{&stakingtypes.MsgDelegate{}, types.ErrUnwrappedMsgType},
+		{&stakingtypes.MsgUndelegate{}, types.ErrUnwrappedMsgType},
+		{&stakingtypes.MsgBeginRedelegate{}, types.ErrUnwrappedMsgType},
+		{&stakingtypes.MsgCancelUnbondingDelegation{}, types.ErrUnwrappedMsgType},
+		// MsgExec that contains staking messages should be rejected
+		{newMsgExecWithStakingMsg(), types.ErrUnwrappedMsgType},
 		// allowed message types
-		{&stakingtypes.MsgEditValidator{}, false},
+		{&stakingtypes.MsgEditValidator{}, nil},
+		{newValidMsgExec(), nil},
 	}
 
 	decorator := NewDropValidatorMsgDecorator(&Keeper{})
 
 	for _, tc := range testCases {
-		res := decorator.IsValidatorRelatedMsg(tc.msg)
-		if tc.expectPass {
-			require.True(t, res)
+		err := decorator.ValidateMsg(tc.msg)
+		if tc.expectErr != nil {
+			require.Error(t, err)
+			require.Equal(t, tc.expectErr, err)
 		} else {
-			require.False(t, res)
+			require.NoError(t, err)
 		}
 	}
 }
