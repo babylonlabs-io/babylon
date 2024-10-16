d064b6d6e1dc629741a2b2b0dcc20269f2323de2
Sam Ricotta <samanthalricotta@gmail.com>
2024-10-16 18:43:53 +0300

    cleanup

--- test/e2e/btc_staking_e2e_test.go
+++ test/e2e/btc_staking_e2e_test.go
@@ -71,12 +71,6 @@ func (s *BTCStakingTestSuite) SetupSuite() {
 	s.NoError(err)
 }
 
-func (s *BTCStakingTestSuite) TearDownSuite() {
-	err := s.configurer.ClearResources()
-	if err != nil {
-	}
-}
-
 // TestCreateFinalityProviderAndDelegation is an end-to-end test for
 // user story 1: user creates finality provider and BTC delegation
 func (s *BTCStakingTestSuite) Test1CreateFinalityProviderAndDelegation() {
