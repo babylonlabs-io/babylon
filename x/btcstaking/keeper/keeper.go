975740febfdab412f41ae064a81a1fb446175c88
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-10-16 13:12:43 -0300

    chore: add comments for bbn block activation heights

--- x/btcstaking/keeper/keeper.go
+++ x/btcstaking/keeper/keeper.go
@@ -111,13 +111,25 @@ func (k Keeper) GetActivationHeight() int64 {
 	// with the test that will be done and the block time
 	// that the validator sets in config.
 	case chaincfg.MainNetParams.Name:
-		// For mainnet considering
-		return 300
-	case chaincfg.TestNet3Params.Name:
+		// For mainnet considering we want 48 hours
+		// at a block time of 10s that would be 17280 blocks
+		// considering the upgrade for Phase-2 will happen at block
+		// 220, the mainnet activation height for btcstaking should
+		// be 17280 + 220.
+		return 17500
+	case chaincfg.SigNetParams.Name:
+		// Signet is used by the devnet testing and 50 blocks
+		// is enough to execute proper checks and verify the voting
+		// power table is not activated before this.
 		return 50
 	case chaincfg.RegressionNetParams.Name:
+		// regtest is only used for internal deployment testing
+		// and 40 blocks is enough to do the proper checks before
+		// the height is reached.
 		return 40
 	default:
+		// Overall btc network configs do not need to wait for activation
+		// as unit tests should not be affected by this.
 		return 0
 	}
 }
