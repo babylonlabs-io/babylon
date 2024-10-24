73585306bf1cebff7ba312565870b15c55103c05
Runchao Han <me@runchao.rocks>
2024-10-24 11:59:46 +1100

    fix merge

--- x/finality/types/params.go
+++ x/finality/types/params.go
@@ -16,6 +16,13 @@ const (
 	DefaultMinPubRand                 = 100
 	DefaultFinalitySigTimeout         = 3
 	DefaultJailDuration               = 24 * 60 * 60 * 1 * time.Second // 1 day
+	// For mainnet considering we want 48 hours
+	// at a block time of 10s that would be 17280 blocks
+	// considering the upgrade for Phase-2 will happen at block
+	// 220, the mainnet activation height for btcstaking should
+	// be 17280 + 220 = 17500.
+	// For now it is set to 1 to avoid breaking dependencies.
+	DefaultFinalityActivationHeight = 1
 )
 
 var (
@@ -33,6 +40,7 @@ func DefaultParams() Params {
 		MinSignedPerWindow:         DefaultMinSignedPerWindow,
 		MinPubRand:                 DefaultMinPubRand,
 		JailDuration:               DefaultJailDuration,
+		FinalityActivationHeight:   DefaultFinalityActivationHeight,
 	}
 }
 
