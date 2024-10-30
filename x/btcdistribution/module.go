517836eec0d48e81e48077294c3acebbe5a99d5a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-10-30 14:48:43 +0400

    fix: init gen

--- x/btcdistribution/module.go
+++ x/btcdistribution/module.go
@@ -57,7 +57,7 @@ func (a AppModuleBasic) RegisterInterfaces(reg cdctypes.InterfaceRegistry) {
 
 // DefaultGenesis returns a default GenesisState for the module, marshalled to json.RawMessage. The default GenesisState need to be defined by the module developer and is primarily used for testing
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return json.RawMessage{}
+	return cdc.MustMarshalJSON(&types.GenesisState{})
 }
 
 // ValidateGenesis used to validate the GenesisState, given in its json.RawMessage form
@@ -124,7 +124,7 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.Ra
 // ExportGenesis returns the module's exported genesis state as raw JSON bytes.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
 	// genState := ExportGenesis(ctx, am.keeper)
-	return []byte{}
+	return cdc.MustMarshalJSON(&types.GenesisState{})
 }
 
 // ConsensusVersion is a sequence number for state-breaking change of the module. It should be incremented on each consensus-breaking change introduced by the module. To avoid wrong/empty versions, the initial version should be set to 1
