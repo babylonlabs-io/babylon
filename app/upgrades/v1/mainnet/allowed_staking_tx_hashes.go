b7133faaf494ae45e8f5600b48497eead16501fb
KonradStaniec <konrad.staniec@gmail.com>
2024-11-13 09:32:32 +0100

    Clean up

--- app/upgrades/v1/mainnet/allowed_staking_tx_hashes.go
+++ app/upgrades/v1/mainnet/allowed_staking_tx_hashes.go
@@ -1,5 +1,6 @@
 package mainnet
 
+// TODO: Specify allowed staking tx hashes for v1 upgrade for mainnet
 const AllowedStakingTxHashesStr = `{
 	"tx_hashes": [
 		"90463a1e0b19137f07c304f2c1c79351fa6f97f1195f1f90dc0b3b57893a350d",
--- app/upgrades/v1/testnet/allowed_staking_tx_hashes.go
+++ app/upgrades/v1/testnet/allowed_staking_tx_hashes.go
@@ -1,5 +1,6 @@
 package testnet
 
+// TODO: Specify allowed staking tx hashes for v1 upgrade for testnet
 const AllowedStakingTxHashesStr = `{
 	"tx_hashes": [
 		"a3572b22571b46b7688fbfe1e8aeee82bb772d52c5ddb069cda361dc33fb4191",
