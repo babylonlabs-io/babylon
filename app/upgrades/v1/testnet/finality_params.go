b90760d1ec2bc20cb81b4ad58f46cdbcd76d0ac5
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-12-09 09:52:08 -0300

    chore: modified finality_activation_height to be 48 hours after the upgrade

--- app/upgrades/v1/testnet/finality_params.go
+++ app/upgrades/v1/testnet/finality_params.go
@@ -1,11 +1,13 @@
 package testnet
 
-// The finality parameters are in the upgrade just because his structure
+// The reason finality parameters are in the upgrade is because its structure
 // had an update and it is possible to overwrite during the upgrade.
-// The finality activation height is when the FP need to have their
-// program ready to start send finality signatures and it could be
-// the same block height where the allow list is expired in this case
-// babylon block height: 26120
+// The finality activation height is when the FPs need to have their
+// programs ready to start sending finality signatures and it is defined
+// to be 48 hours after the upgrade, the upgrade will happen close to 
+// block 200 and 48 hours of blocks with 10 seconds block time gives 17280 
+// blocks. In this case the finality activation block heigth will be set
+// as 17480 = 17280 (48hrs worth of blocks) + ~200 (blocks of TGE).
 const FinalityParamStr = `{
   "max_active_finality_providers": 100,
   "signed_blocks_window": 100,
@@ -13,5 +15,5 @@ const FinalityParamStr = `{
   "min_signed_per_window": "0.1",
   "min_pub_rand": 500,
   "jail_duration": "86400s",
-  "finality_activation_height": 26120
+  "finality_activation_height": 17480
 }`
