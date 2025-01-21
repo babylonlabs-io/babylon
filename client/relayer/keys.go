c95b01e63cd05116f3d7795f7ed4031346560db5
Lazar <lukic.lazar@yahoo.com>
2025-01-21 20:56:52 +0100

    add notice headers

--- client/relayer/keys.go
+++ client/relayer/keys.go
@@ -1,3 +1,6 @@
+// This file is derived from the Cosmos Relayer repository (https://github.com/cosmos/relayer),
+// originally licensed under the Apache License, Version 2.0.
+
 package relayerclient
 
 import (
