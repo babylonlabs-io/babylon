19407c798693e79b70c30beb40d0dcb2dcc20275
KonradStaniec <konrad.staniec@gmail.com>
2025-02-07 15:10:50 +0100

    add rc.5 tesnet upgrade handler (#493)
    Adds testnet v1rc5 handler
    
    Note: handler needs to live on main as later when we will be upgrading
    testnet to v2, the upgrade will need to be made from the latest upgrade
    on the testnet

---
+++ app/upgrades/v1rc5/testnet/upgrade.go
@@ -0,0 +1,39 @@
+package testnet
+
+import (
+	"context"
+	"fmt"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/babylonlabs-io/babylon/app/keepers"
+	"github.com/babylonlabs-io/babylon/app/upgrades"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+const (
+	UpgradeName = "v1rc5"
+)
+
+func CreateUpgrade() upgrades.Upgrade {
+	return upgrades.Upgrade{
+		UpgradeName:          UpgradeName,
+		CreateUpgradeHandler: CreateUpgradeHandler(),
+	}
+}
+
+// CreateUpgradeHandler upgrade handler for launch.
+func CreateUpgradeHandler() upgrades.UpgradeHandlerCreator {
+	return func(mm *module.Manager, cfg module.Configurator, keepers *keepers.AppKeepers) upgradetypes.UpgradeHandler {
+		return func(context context.Context, _plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			ctx := sdk.UnwrapSDKContext(context)
+
+			migrations, err := mm.RunMigrations(ctx, cfg, fromVM)
+			if err != nil {
+				return nil, fmt.Errorf("failed to run migrations: %w", err)
+			}
+
+			return migrations, nil
+		}
+	}
+}
