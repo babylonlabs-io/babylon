b43a578bc9655669bc1054f844f4b2f3b8c2c310
KonradStaniec <konrad.staniec@gmail.com>
2025-02-12 18:24:30 +0100

    add staker address to the creation event (#515)
    Feature request by Web team: 
    - creation event should contain staker address

--- x/btcstaking/types/events.go
+++ x/btcstaking/types/events.go
@@ -109,6 +109,7 @@ func NewBtcDelCreationEvent(
 		UnbondingTime:             strconv.FormatUint(uint64(btcDel.UnbondingTime), 10),
 		UnbondingTx:               hex.EncodeToString(btcDel.BtcUndelegation.UnbondingTx),
 		NewState:                  BTCDelegationStatus_PENDING.String(),
+		StakerAddr:                btcDel.StakerAddr,
 	}
 }
 
