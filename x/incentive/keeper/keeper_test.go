a901dbd00f989d43718efda3b5fcef103bfc6f10
KonradStaniec <konrad.staniec@gmail.com>
2025-03-03 08:53:21 +0100

    refund fee to correct receipient (#594)
    - in case of fee granter refund fee to the granter
    
    (cherry picked from commit bd8bf36ad07f81d9b1af071c67fd333299b394f6)

---
+++ x/incentive/keeper/keeper_test.go
@@ -0,0 +1,75 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdkmath "cosmossdk.io/math"
+	keepertest "github.com/babylonlabs-io/babylon/testutil/keeper"
+	"github.com/babylonlabs-io/babylon/x/incentive/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// mockFeeTx implements the sdk.FeeTx interface for testing
+type mockFeeTx struct {
+	fee      sdk.Coins
+	feePayer []byte
+	granter  []byte
+}
+
+func (tx mockFeeTx) GetFee() sdk.Coins {
+	return tx.fee
+}
+
+func (tx mockFeeTx) FeePayer() []byte {
+	return tx.feePayer
+}
+
+func (tx mockFeeTx) FeeGranter() []byte {
+	return tx.granter
+}
+
+// Additional methods required by sdk.FeeTx interface
+func (tx mockFeeTx) GetGas() uint64     { return 0 }
+func (tx mockFeeTx) GetMsgs() []sdk.Msg { return nil }
+func (tx mockFeeTx) GetMsgsV2() ([]protoreflect.ProtoMessage, error) {
+	return nil, nil
+}
+
+func TestRefundToCorrectRecipient(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	bankKeeper := types.NewMockBankKeeper(ctrl)
+
+	iKeeper, ctx := keepertest.IncentiveKeeper(t, bankKeeper, nil, nil)
+
+	// Create test addresses
+	feePayer := []byte("feePayer")
+	feeGranter := []byte("feeGranter")
+
+	mockFeeTxPayer := mockFeeTx{
+		fee:      sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(100))),
+		feePayer: feePayer,
+		granter:  nil,
+	}
+
+	bankKeeper.EXPECT().SendCoinsFromModuleToAccount(gomock.Any(), gomock.Any(), feePayer, gomock.Any()).Return(nil)
+
+	err := iKeeper.RefundTx(ctx, mockFeeTxPayer)
+	require.NoError(t, err)
+
+	mockFeeTxGranter := mockFeeTx{
+		fee:      sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(100))),
+		feePayer: feePayer,
+		granter:  feeGranter,
+	}
+
+	bankKeeper.EXPECT().SendCoinsFromModuleToAccount(gomock.Any(), gomock.Any(), feeGranter, gomock.Any()).Return(nil)
+
+	err = iKeeper.RefundTx(ctx, mockFeeTxGranter)
+	require.NoError(t, err)
+}
