af5872cec3c7d386c2366b1b42d9a517d54ceceb
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-03-06 17:25:50 -0300

    chore: backport #648 to cl

--- x/btcstaking/client/cli/query_params.go
+++ x/btcstaking/client/cli/query_params.go
@@ -35,3 +35,34 @@ func CmdQueryParams() *cobra.Command {
 	return cmd
 }
 
+func CmdQueryParamsVersions() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params-versions",
+		Short: "shows all the parameters of the module with version",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+			res, err := queryClient.ParamsVersions(cmd.Context(), &types.QueryParamsVersionsRequest{
+				Pagination: pageReq,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
