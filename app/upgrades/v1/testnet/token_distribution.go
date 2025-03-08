8bed98fb54151a5ceabfdf4e8ec9c14d23b4a577
Hiep Mai <maiquanghiepdl@gmail.com>
2025-03-08 13:55:23 +0700

    Correct the address

--- app/upgrades/v1/testnet/token_distribution.go
+++ app/upgrades/v1/testnet/token_distribution.go
@@ -4,102 +4,102 @@ package testnet
 const TokensDistributionStr = `{
   "token_distribution": [
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1ut28z83nyfq9ntnys0yd7sq367w9q7fmet0zsj",
       "amount": 1018474
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1y5u9ndam9vzmmrvxgdk0vpxmlszv6dvddr0hrw",
       "amount": 1019994
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1gsp0q9q96gukakzpa57x42v95tkf2wylmcx3sx",
       "amount": 1027755
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1tl0gf67e00eawl6rx5zsqhpq73w7hmqc6ule5m",
       "amount": 1030363
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1m0epjkdd7rrg8thqj2vk52yhps7j27wrgf067v",
       "amount": 1013032
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1rauscuw7qn8s0j9g0h8acyadxzk47p39pq6988",
       "amount": 1000236
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn16ctr3sqknklnshacrj0l5pl9sy53q8te2s7nrv",
       "amount": 1003008
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn125f3f6l2uec2hpern06dxunfxenqa4jtwcudz0",
       "amount": 1019303
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1ywejqscjyw03ld7mh8fgrkpqs78jft2h0lgtqm",
       "amount": 1012316
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn120t4tw9le3h6rp8dcrkztqxh29md7twjdfqpdv",
       "amount": 1007956
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1gtkhza6k8gq3tgz0hmgktqppn520ustdchv3un",
       "amount": 1024327
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1ftwjlgwyulxufaluxw62y67h898hz8dffc08ue",
       "amount": 1013915
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1tw6678k2qhz5xs0wrccu6wmwjpmpa34p9npnhx",
       "amount": 1030815
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn13yevasftn0p2k57anze48pllw34y4ymvt5ualm",
       "amount": 1021033
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1eetk3kpj7dkg8xzl6m5e7jqtd5vpzra3hvs5kc",
       "amount": 1017259
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn12uwj4uy084e6xc6xchfwm3cu7r707lwusztl69",
       "amount": 1026039
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1d0j9trc4phg2e6j3t2hkd3n9vgc5qpzyfrev78",
       "amount": 1021393
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn19ujaa2jk8740mrdkz4dl75gjrcwepkz4edk93z",
       "amount": 1007640
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn1yhwkqalgmrudjzjyex0z97pn39zy8jrc3an0qh",
       "amount": 1006794
     },
     {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
+      "address_sender": "bbn1m7ht7cz30cqplnuepf9rp9gschfe0q5zzd8vjc",
       "address_receiver": "bbn13xn5hy96s4zhm9kwfm69z6xqsuzsqqmzwrx22z",
       "amount": 1005165
     }
