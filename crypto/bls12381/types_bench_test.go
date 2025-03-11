155d7274d61224a1530c6654a0bb0490a29eec7d
Fangyu Gai <greferry@gmail.com>
2025-03-11 17:33:28 +0800

    enable group check

---
+++ crypto/bls12381/types_bench_test.go
@@ -0,0 +1,61 @@
+package bls12381
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+)
+
+func BenchmarkVerifyCompressed(b *testing.B) {
+	// Generate random messages, keys and signatures
+	msg := make([]byte, 32)
+	rand.Read(msg)
+
+	// Generate a random key pair
+	sk, pk := GenKeyPair()
+
+	// Create a signature
+	sig := Sign(sk, msg)
+
+	b.Run(fmt.Sprintf("WithSigGroupCheck"), func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			dummySig := new(BlsSig)
+			dummySig.VerifyCompressed(sig, true, pk, false, msg, DST)
+		}
+	})
+
+	b.Run(fmt.Sprintf("WithoutSigGroupCheck"), func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			dummySig := new(BlsSig)
+			dummySig.VerifyCompressed(sig, false, pk, false, msg, DST)
+		}
+	})
+}
+
+func BenchmarkAggregateCompressed(b *testing.B) {
+	// Generate random public keys
+	size := 100
+	pks := make([][]byte, 0, size)
+	for i := 0; i < size; i++ {
+		_, pk := GenKeyPair()
+		pks = append(pks, pk.Bytes())
+	}
+
+	b.Run(fmt.Sprintf("WithGroupCheck"), func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			aggPk := new(BlsMultiPubKey)
+			aggPk.AggregateCompressed(pks, true) // With groupcheck
+		}
+	})
+
+	b.Run(fmt.Sprintf("WithoutGroupCheck"), func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			aggPk := new(BlsMultiPubKey)
+			aggPk.AggregateCompressed(pks, false) // Without groupcheck
+		}
+	})
+}
