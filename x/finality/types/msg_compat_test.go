9bcdbadbd0d91ee90e47930a3a737ff3fc119c29
Runchao Han <me@runchao.rocks>
2025-03-27 15:04:42 +1100

    fix

--- x/finality/types/msg_compat_test.go
+++ x/finality/types/msg_compat_test.go
@@ -16,7 +16,7 @@ var (
 	MSG2 = `{"msg": "{\"signer\":\"bbn1llns56v0844chclvtsnttnjmpxny40sn64unt3\",\"fp_btc_pk\":\"2877658a1e7608fe84543a75ca879334bb8f2e7aecae71adb7767192bda50739\",\"block_height\":595992,\"pub_rand\":\"RrBkeGyIcHWzEhypnsWfKJHaWiVjXcRz92r1h206+zg=\",\"proof\":{\"total\":70000,\"index\":27148,\"leaf_hash\":\"CwoZFuS7U7rMcB34FbYjE86gVsH1kRmK5cVZ1HLFalo=\",\"aunts\":[\"71m1CuA2KFTchCMlf/tFJCmmOt/k/yTuA0Dpf+afqgQ=\",\"NCKhl9MsEawZ780KGn4sfqTF3Bo43wXXcW+LgSDybY4=\",\"59sqIWfTeAHZznLN6zN2lPApzamkQptwsTRO17BTVoM=\",\"3TaxNa6G5F6K/T3w0EcXinRfHMtInthnfzWhcBZ/i0k=\",\"JUYdT96QwZNxb2cyisY62nQ+oglmuak8fJXhFMgXj6g=\",\"eMQMyUubJX8MWYUtsxSq5qOHg5ximWd6h7hrt1PSOxM=\",\"Al75nCG9D6KHCIlQBinulrmoqf4zziCX1s1KrvMgSzM=\",\"v4Ep+U2Q5I/vUlx77brPgJESIZHCMKERV0hQqRwUYKk=\",\"9xtU7fYolEW0+6Tl8d+1G8WM+XkchhZ9wPGaUQ39bT8=\",\"/WkTLgS47o0vsNn6aeK4XtWui0+S/gdGB0qrzhQfu28=\",\"6Fm78utdm1M1W0DrCKPp1E6edDhvEy+ssCKG1xLRZvE=\",\"BbZ68FXOCHk7i2Z75VnMtugQPDK9wH6ioOVSZUyatpM=\",\"9U/alqJcsDpqjZCDnRnlbjYK4SsqwPdU8xoC2nQxZlw=\",\"EfajX5SWuW2YREjBMlzBfLhdgp+O8ZWRCcbMvsY0M+I=\",\"cdp93LsDsyqku102jI/DaHcxDnIkLDSwR6PNNs/Djtg=\",\"1m1ztn/R0T8tqtH0iVFMPF2JB+PmvkDdEdNWjiu3Qug=\",\"FI7Baxp7Q3kY6W+uQyPptLlLDeK7O6eBdWGg7fYw9W8=\"]},\"block_app_hash\":\"JhNlKmsP1MPrOnRR055vnmd+yzZJ1rMR6FN7QCZbRyU=\",\"finality_sig\":\"TrhZZNt69IsHWeXCDS4ylXTUpQHrYn9XuOHEj6pZTG4=\"}"}`
 )
 
-func TestParseTestData(t *testing.T) {
+func TestEOTSCompat(t *testing.T) {
 	commitBytes, err := base64.StdEncoding.DecodeString("HAzJxMSUIBTMTkzXZv3kzoCKZxuhtRYcf2pN1mSWh7E=")
 	require.NoError(t, err)
 	prCommit := &types.PubRandCommit{
