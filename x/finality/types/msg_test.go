66713e0cd9823da6e8123a9fb41b87a72ef584d3
Runchao Han <me@runchao.rocks>
2025-03-27 11:21:04 +1100

    init

--- x/finality/types/msg_test.go
+++ x/finality/types/msg_test.go
@@ -1,6 +1,8 @@
 package types_test
 
 import (
+	"encoding/json"
+	fmt "fmt"
 	"math/rand"
 	"testing"
 
@@ -12,6 +14,9 @@ import (
 
 func FuzzMsgAddFinalitySig(f *testing.F) {
 	datagen.AddRandomSeedsToFuzzer(f, 10)
+
+	prCommitList := []*types.PubRandCommit{}
+	msgList := []*types.MsgAddFinalitySig{}
 
 	f.Fuzz(func(t *testing.T, seed int64) {
 		r := rand.New(rand.NewSource(seed))
@@ -37,10 +42,19 @@ func FuzzMsgAddFinalitySig(f *testing.F) {
 			Commitment:  randListInfo.Commitment,
 		}
 
+		prCommitList = append(prCommitList, prCommit)
+		msgList = append(msgList, msg)
+
 		// verify the finality signature message
 		err = types.VerifyFinalitySig(msg, prCommit)
 		require.NoError(t, err)
 	})
+
+	testStrJson, _ := json.Marshal(prCommitList)
+	fmt.Println(string(testStrJson))
+	fmt.Println("--------------------------------")
+	testStrJson, _ = json.Marshal(msgList)
+	fmt.Println(string(testStrJson))
 }
 
 func FuzzMsgCommitPubRandList(f *testing.F) {
