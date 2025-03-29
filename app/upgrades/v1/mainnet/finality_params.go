b016819a5efb98e497cca5447caa5408ce0969dd
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-03-29 15:27:09 -0300

    chore: update min_signed_per_window from 0.3 to 02

--- app/upgrades/v1/mainnet/finality_params.go
+++ app/upgrades/v1/mainnet/finality_params.go
@@ -4,7 +4,7 @@ const FinalityParamStr = `{
   "max_active_finality_providers": 60,
   "signed_blocks_window": 10000,
   "finality_sig_timeout": 3,
-  "min_signed_per_window": "0.3",
+  "min_signed_per_window": "0.2",
   "min_pub_rand": 8192,
   "jail_duration": "600s",
   "finality_activation_height": 17500
