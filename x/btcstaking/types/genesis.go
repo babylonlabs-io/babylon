ac6c350d4ab2fff8099380965c68fb15f23b7c8d
Tom <54514587+GAtom22@users.noreply.github.com>
2025-04-21 10:45:50 -0300

    chore(x/btcstaking): add collections to import/export genesis logic (#828)

--- x/btcstaking/types/genesis.go
+++ x/btcstaking/types/genesis.go
@@ -1,9 +1,13 @@
 package types
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"slices"
+	"sort"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
@@ -38,7 +42,14 @@ func (gs GenesisState) Validate() error {
 			return err
 		}
 	}
-	return nil
+
+	if gs.LargestBtcReorg != nil {
+		if err := gs.LargestBtcReorg.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return gs.validateAllowedStakingTxHashes()
 }
 
 // GenesisStateFromAppState returns x/btcstaking GenesisState given raw application
@@ -51,4 +62,62 @@ func GenesisStateFromAppState(cdc codec.Codec, appState map[string]json.RawMessa
 	}
 
 	return genesisState
+}
+
+type AllowedStakingTxHashStr string
+
+func (h AllowedStakingTxHashStr) Validate() error {
+	// hashes are hex encoded for better readability
+	bz, err := hex.DecodeString(string(h))
+	if err != nil {
+		return fmt.Errorf("error decoding tx hash: %w", err)
+	}
+	// NewHash validates hash size
+	if _, err := chainhash.NewHash(bz); err != nil {
+		return err
+	}
+	return nil
+}
+
+// validateAllowedStakingTxHashes validates there're no duplicate entries
+// and the hash has the corresponding size
+func (gs GenesisState) validateAllowedStakingTxHashes() error {
+	hashes := make(map[string]bool)
+	for _, hStr := range gs.AllowedStakingTxHashes {
+		if _, exists := hashes[hStr]; exists {
+			return fmt.Errorf("duplicate staking tx hash: %s", hStr)
+		}
+		hashes[hStr] = true
+		h := AllowedStakingTxHashStr(hStr)
+		if err := h.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Helper function to sort slices to get a deterministic
+// result on the tests
+func SortData(gs *GenesisState) {
+	sort.Slice(gs.FinalityProviders, func(i, j int) bool {
+		return gs.FinalityProviders[i].Addr < gs.FinalityProviders[j].Addr
+	})
+
+	sort.Slice(gs.BtcDelegations, func(i, j int) bool {
+		return gs.BtcDelegations[i].StakerAddr < gs.BtcDelegations[j].StakerAddr
+	})
+
+	sort.Slice(gs.BlockHeightChains, func(i, j int) bool {
+		return gs.BlockHeightChains[i].BlockHeightBbn < gs.BlockHeightChains[j].BlockHeightBbn
+	})
+
+	sort.Slice(gs.BtcDelegators, func(i, j int) bool {
+		return gs.BtcDelegators[i].String() < gs.BtcDelegators[j].String()
+	})
+
+	sort.Slice(gs.Events, func(i, j int) bool {
+		return gs.Events[i].Idx < gs.Events[j].Idx
+	})
+
+	slices.Sort(gs.AllowedStakingTxHashes)
 }
