ee687701b1921b55660c76464e8e5421f4f0e3e9
samricotta <37125168+samricotta@users.noreply.github.com>
2025-04-23 16:31:58 +0300

    Update protoc-swagger-gen.sh

--- proto/scripts/protoc-swagger-gen.sh
+++ proto/scripts/protoc-swagger-gen.sh
@@ -6,7 +6,7 @@ mkdir -p ./tmp-swagger-gen
 cd proto
 proto_dirs=$(find ./babylon -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  # generate OpenAPI v2 files (filter query files)
+  # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
   if [[ ! -z "$query_file" ]]; then
     echo "Generating Swagger for $query_file"
@@ -17,8 +17,10 @@ for dir in $proto_dirs; do
 done
 
 cd ..
-# combine OpenAPI v2 files
+# combine swagger files
+# uses nodejs package `swagger-combine`.
+# all the individual swagger files need to be configured in `config.json` for merging
 swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
 
-# clean OpenAPI v2 files
+# clean swagger files
 rm -rf ./tmp-swagger-gen
