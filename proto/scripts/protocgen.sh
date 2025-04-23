86480b8fbfdf9605458dc0b5682a286938496680
samricotta <37125168+samricotta@users.noreply.github.com>
2025-04-23 16:31:58 +0300

    fix: downgrade proto to 0.14 (#838)
    Downgrade to `0.14` as there needs to be some investigation against why
    we should upgrade to `0.16`

--- proto/scripts/protocgen.sh
+++ proto/scripts/protocgen.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 cd proto
 proto_dirs=$(find ./babylon -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 
-buf dep update
+buf mod update
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep go_package $file &>/dev/null; then
