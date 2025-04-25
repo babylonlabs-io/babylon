99e76b91bcfd66654075c948015671fa2afac71f
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-04-25 17:56:03 -0300

    fix: wire btcstaking to btc light client hooks

--- cmd/babylond/cmd/cmd_test.go
+++ cmd/babylond/cmd/cmd_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/babylonlabs-io/babylon/app"
 	"github.com/babylonlabs-io/babylon/cmd/babylond/cmd"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 )
@@ -18,6 +19,7 @@ func TestInitCmd(t *testing.T) {
 		"init",     // Test the init cmd
 		"app-test", // Moniker
 		fmt.Sprintf("--%s=%s", cli.FlagOverwrite, "true"), // Overwrite genesis.json, in case it already exists
+		fmt.Sprintf("--%s=%s", flags.FlagHome, t.TempDir()),
 		"--no-bls-password",
 	})
 
