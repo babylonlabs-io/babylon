c801d4d5606c8a539cae20a7c1097a56ad0dc0ab
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-04-25 16:12:21 -0300

    chore: split scenario of slashed finality provider

--- test/replay/finality_test.go
+++ test/replay/finality_test.go
@@ -51,31 +51,105 @@ func TestResumeFinalityOfSlashedFp(t *testing.T) {
 	scn := NewStandardScenario(d)
 	scn.InitScenario(2, 1) // 2 fps, 1 del each
 
-	lastVotedBlkHeight, numBlocksFinalized := uint64(0), uint64(2)
-
-	// finalize first 2 blocks, where both vote
-	for blkHeight := scn.activationHeight; blkHeight < scn.activationHeight+numBlocksFinalized; blkHeight++ {
-		bl := d.GetIndexedBlock(blkHeight)
-		require.Equal(t, bl.Finalized, false)
-
-		for _, fp := range scn.finalityProviders {
-			fp.CastVote(blkHeight)
-		}
-
-		d.GenerateNewBlockAssertExecutionSuccess()
-
-		bl = d.GetIndexedBlock(blkHeight)
-		require.Equal(t, bl.Finalized, true)
-		lastVotedBlkHeight = blkHeight
-	}
+	numBlocksFinalized := uint64(2)
+	lastVotedBlkHeight := scn.FinalityFinalizeBlocks(scn.activationHeight, numBlocksFinalized)
 
 	// one fp continues to vote, but the one to be jailed one does not vote
-	jailedFp := scn.finalityProviders[1]
+	indexSlashFp := 1
+	lastFinalizedBlkHeight := lastVotedBlkHeight
+
+	// vote only with one fp, to halt finality
+	for i := uint64(0); i < numBlocksFinalized; i++ {
+		lastVotedBlkHeight++
+		for _, fp := range scn.finalityProviders[:indexSlashFp] {
+			fp.CastVote(lastVotedBlkHeight)
+		}
+
+		d.GenerateNewBlock()
+
+		bl := d.GetIndexedBlock(lastVotedBlkHeight)
+		require.Equal(t, bl.Finalized, false)
+	}
+
+	// verifes that the fp is still not jailed or slashed
+	slashFp := scn.finalityProviders[indexSlashFp]
+	fp := d.GetFp(*slashFp.BTCPublicKey())
+	require.False(t, fp.Jailed)
+	require.False(t, fp.IsSlashed())
+
+	vp := d.App.FinalityKeeper.GetVotingPower(d.Ctx(), *slashFp.BTCPublicKey(), lastFinalizedBlkHeight)
+	require.NotZero(t, vp)
+
+	badBlock := d.GetIndexedBlock(lastVotedBlkHeight - 1)
+	goodBlock := d.GetIndexedBlock(lastVotedBlkHeight)
+	// fp slashed with bogus vote
+	slashFp.CastVoteForHash(lastVotedBlkHeight, badBlock.AppHash)
+	slashFp.CastVoteForHash(lastVotedBlkHeight, goodBlock.AppHash)
+
+	d.GenerateNewBlockAssertExecutionSuccess()
+
+	slashedFp := d.GetFp(*slashFp.BTCPublicKey())
+	require.False(t, slashedFp.Jailed)
+	require.True(t, slashedFp.IsSlashed())
+
+	// send gov proposal to resume finality
+	prop := ftypes.MsgResumeFinalityProposal{
+		Authority:     appparams.AccGov.String(),
+		FpPksHex:      []string{slashedFp.BtcPk.MarshalHex()},
+		HaltingHeight: uint32(lastFinalizedBlkHeight + 1), // fp voted in the last finalized
+	}
+	d.GovPropWaitPass(&prop)
+
+	d.GenerateNewBlock()
+	// check that the blocks got finalized
+	for blkHeight := lastFinalizedBlkHeight + 1; blkHeight <= lastVotedBlkHeight; blkHeight++ {
+		bl := d.GetIndexedBlock(blkHeight)
+		require.Equal(t, bl.Finalized, true)
+
+		vp := d.App.FinalityKeeper.GetVotingPower(d.Ctx(), *slashFp.BTCPublicKey(), blkHeight)
+		require.Zero(t, vp)
+	}
+
+	// the fp in the voting power distribution cache should still be marked as slashed
+	vpDstCache := d.GetVotingPowerDistCache(d.GetLastFinalizedBlock().Height)
+	for _, vpFp := range vpDstCache.FinalityProviders {
+		if vpFp.BtcPk.Equals(slashFp.BTCPublicKey()) {
+			require.True(d.t, vpFp.IsSlashed)
+			require.Zero(d.t, vpFp.TotalBondedSat)
+			continue
+		}
+
+		require.False(d.t, vpFp.IsJailed)
+		require.False(d.t, vpFp.IsSlashed)
+		require.NotZero(d.t, vpFp.TotalBondedSat)
+	}
+
+	// continue to be slashed status in btcstaking
+	slashedFp = d.GetFp(*slashFp.BTCPublicKey())
+	require.True(t, slashedFp.IsSlashed())
+}
+
+func TestResumeFinalityOfJailedSlashedFp(t *testing.T) {
+	t.Parallel()
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	d := NewBabylonAppDriverTmpDir(r, t)
+	d.GenerateNewBlock()
+
+	scn := NewStandardScenario(d)
+	scn.InitScenario(2, 1) // 2 fps, 1 del each
+
+	// finalize first 2 blocks, where both vote
+	numBlocksFinalized := uint64(2)
+	lastVotedBlkHeight := scn.FinalityFinalizeBlocks(scn.activationHeight, numBlocksFinalized)
+
+	// one fp continues to vote, but the one to be jailed one does not vote
+	indexSlashFp := 1
+	jailedFp := scn.finalityProviders[indexSlashFp]
 	lastFinalizedBlkHeight := lastVotedBlkHeight
 
 	for {
 		lastVotedBlkHeight++
-		for _, fp := range scn.finalityProviders[:1] {
+		for _, fp := range scn.finalityProviders[:indexSlashFp] {
 			fp.CastVote(lastVotedBlkHeight)
 		}
 
@@ -112,6 +186,9 @@ func TestResumeFinalityOfSlashedFp(t *testing.T) {
 	for blkHeight := lastFinalizedBlkHeight + 1; blkHeight <= lastVotedBlkHeight; blkHeight++ {
 		bl := d.GetIndexedBlock(blkHeight)
 		require.Equal(t, bl.Finalized, true)
+
+		vp := d.App.FinalityKeeper.GetVotingPower(d.Ctx(), *jailedFp.BTCPublicKey(), blkHeight)
+		require.Zero(t, vp)
 	}
 
 	// the fp in the voting power distribution cache should still be marked as slashed
