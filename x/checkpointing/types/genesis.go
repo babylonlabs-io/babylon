259c685f1e97605c6646ec731ad3cde1f857443d
Tom <54514587+GAtom22@users.noreply.github.com>
2025-04-25 08:21:36 -0300

    chore(x/checkpointing): Add import/export genesis logic (#840)

--- x/checkpointing/types/genesis.go
+++ x/checkpointing/types/genesis.go
@@ -2,10 +2,10 @@ package types
 
 import (
 	"encoding/json"
-	"errors"
-	fmt "fmt"
+	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	tmjson "github.com/cometbft/cometbft/libs/json"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -14,6 +14,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/babylonlabs-io/babylon/v2/crypto/bls12381"
+	"github.com/babylonlabs-io/babylon/v2/types"
 )
 
 // DefaultGenesis returns the default Capability genesis state
@@ -24,19 +25,23 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
-	addresses := make(map[string]struct{}, 0)
-	for _, gk := range gs.GenesisKeys {
-		if _, exists := addresses[gk.ValidatorAddress]; exists {
-			return errors.New("duplicate genesis key")
-		}
-		addresses[gk.ValidatorAddress] = struct{}{}
-		err := gk.Validate()
-		if err != nil {
-			return err
-		}
+	if err := types.ValidateEntries(gs.GenesisKeys, func(gk *GenesisKey) string { return gk.ValidatorAddress }); err != nil {
+		return err
 	}
 
-	return nil
+	if err := types.ValidateEntries(gs.ValidatorSets, func(e *ValidatorSetEntry) uint64 { return e.EpochNumber }); err != nil {
+		return err
+	}
+
+	return types.ValidateEntries(
+		gs.Checkpoints,
+		func(chkpt *RawCheckpointWithMeta) uint64 {
+			if chkpt.Ckpt == nil {
+				return 0
+			}
+			return chkpt.Ckpt.EpochNum
+		},
+	)
 }
 
 func NewGenesisKey(delAddr sdk.ValAddress, blsPubKey *bls12381.PublicKey, pop *ProofOfPossession, pubkey cryptotypes.PubKey) (*GenesisKey, error) {
@@ -106,4 +111,27 @@ func GenTxMessageValidatorWrappedCreateValidator(msgs []sdk.Msg) error {
 	}
 
 	return nil
+}
+
+func (gk *ValidatorSetEntry) Validate() error {
+	return gk.ValidatorSet.Validate()
+}
+
+// Helper function to sort slices to get a deterministic
+// result on the tests
+func SortData(gs *GenesisState) {
+	sort.Slice(gs.GenesisKeys, func(i, j int) bool {
+		return gs.GenesisKeys[i].ValidatorAddress < gs.GenesisKeys[j].ValidatorAddress
+	})
+
+	sort.Slice(gs.ValidatorSets, func(i, j int) bool {
+		return gs.ValidatorSets[i].EpochNumber < gs.ValidatorSets[j].EpochNumber
+	})
+
+	sort.Slice(gs.Checkpoints, func(i, j int) bool {
+		if gs.Checkpoints[i].Ckpt != nil && gs.Checkpoints[j].Ckpt != nil {
+			return gs.Checkpoints[i].Ckpt.EpochNum < gs.Checkpoints[j].Ckpt.EpochNum
+		}
+		return gs.Checkpoints[i].PowerSum < gs.Checkpoints[j].PowerSum
+	})
 }
