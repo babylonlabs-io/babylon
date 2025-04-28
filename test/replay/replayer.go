c6f83f332670b2f85efc6b8ed010f50ef6261f7a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-04-28 12:44:53 -0300

    fix: add checks for slashed fp in gov resume finality (#829)
    Prior to this, any slashed FP at the latest height wouldn't be jailed
    and the proposal would fail
    Add checks for FP slash and only set in vp distribution cache to jailed
    if it wasn't already slashed

---
+++ test/replay/replayer.go
@@ -0,0 +1,112 @@
+package replay
+
+import (
+	"testing"
+
+	"cosmossdk.io/log"
+	babylonApp "github.com/babylonlabs-io/babylon/app"
+	appsigner "github.com/babylonlabs-io/babylon/app/signer"
+	dbmc "github.com/cometbft/cometbft-db"
+	cs "github.com/cometbft/cometbft/consensus"
+	cometlog "github.com/cometbft/cometbft/libs/log"
+	"github.com/cometbft/cometbft/mempool"
+	"github.com/cometbft/cometbft/proxy"
+	sm "github.com/cometbft/cometbft/state"
+	"github.com/cometbft/cometbft/store"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/stretchr/testify/require"
+)
+
+type BlockReplayer struct {
+	BlockExec *sm.BlockExecutor
+	LastState sm.State
+}
+
+func NewBlockReplayer(t *testing.T, nodeDir string) *BlockReplayer {
+	_, doc := getGenDoc(t, nodeDir)
+
+	genDoc, err := doc.ToGenesisDoc()
+	require.NoError(t, err)
+
+	state, err := sm.MakeGenesisState(genDoc)
+	require.NoError(t, err)
+
+	stateStore := sm.NewStore(dbmc.NewMemDB(), sm.StoreOptions{
+		DiscardABCIResponses: false,
+	})
+
+	if err := stateStore.Save(state); err != nil {
+		panic(err)
+	}
+
+	blsSigner, err := appsigner.InitBlsSigner(nodeDir)
+	require.NoError(t, err)
+	require.NotNil(t, blsSigner)
+
+	appOptions := NewAppOptionsWithFlagHome(nodeDir)
+	baseAppOptions := server.DefaultBaseappOptions(appOptions)
+	tmpApp := babylonApp.NewBabylonApp(
+		log.NewNopLogger(),
+		dbm.NewMemDB(),
+		nil,
+		true,
+		map[int64]bool{},
+		0,
+		blsSigner,
+		appOptions,
+		babylonApp.EmptyWasmOpts,
+		baseAppOptions...,
+	)
+
+	cmtApp := server.NewCometABCIWrapper(tmpApp)
+	procxyCons := proxy.NewMultiAppConn(
+		proxy.NewLocalClientCreator(cmtApp),
+		proxy.NopMetrics(),
+	)
+	err = procxyCons.Start()
+	require.NoError(t, err)
+
+	blockStore := store.NewBlockStore(dbmc.NewMemDB())
+
+	blockExec := sm.NewBlockExecutor(
+		stateStore,
+		cometlog.TestingLogger(),
+		procxyCons.Consensus(),
+		&mempool.NopMempool{},
+		sm.EmptyEvidencePool{},
+		blockStore,
+	)
+	require.NotNil(t, blockExec)
+
+	hs := cs.NewHandshaker(
+		stateStore,
+		state,
+		blockStore,
+		genDoc,
+	)
+
+	require.NotNil(t, hs)
+	hs.SetLogger(cometlog.TestingLogger())
+	err = hs.Handshake(procxyCons)
+	require.NoError(t, err)
+
+	state, err = stateStore.Load()
+	require.NoError(t, err)
+	require.NotNil(t, state)
+
+	return &BlockReplayer{
+		BlockExec: blockExec,
+		LastState: state,
+	}
+}
+
+func (r *BlockReplayer) ReplayBlocks(t *testing.T, blocks []FinalizedBlock) {
+	for _, block := range blocks {
+		blockID, _ := getBlockId(t, block.Block)
+		state, err := r.BlockExec.ApplyVerifiedBlock(r.LastState, blockID, block.Block)
+		require.NoError(t, err)
+		require.NotNil(t, state)
+		r.LastState = state.Copy()
+	}
+}
--- x/finality/keeper/gov_test.go
+++ x/finality/keeper/gov_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/babylonlabs-io/babylon/testutil/datagen"
 	keepertest "github.com/babylonlabs-io/babylon/testutil/keeper"
 	bbntypes "github.com/babylonlabs-io/babylon/types"
+	bstypes "github.com/babylonlabs-io/babylon/x/btcstaking/types"
 	"github.com/babylonlabs-io/babylon/x/finality/keeper"
 	"github.com/babylonlabs-io/babylon/x/finality/types"
 )
@@ -68,6 +69,9 @@ func TestHandleResumeFinalityProposal(t *testing.T) {
 
 	// create a resume finality proposal to jail the last fp
 	bsKeeper.EXPECT().JailFinalityProvider(ctx, gomock.Any()).Return(nil).AnyTimes()
+	bsKeeper.EXPECT().GetFinalityProvider(gomock.Any(), gomock.Any()).Return(&bstypes.FinalityProvider{
+		Jailed: false,
+	}, nil).AnyTimes()
 	err := fKeeper.HandleResumeFinalityProposal(ctx, publicKeysToHex(activeFpPks[1:]), uint32(haltingHeight))
 	require.NoError(t, err)
 
@@ -77,6 +81,133 @@ func TestHandleResumeFinalityProposal(t *testing.T) {
 		ib, err := fKeeper.GetBlock(ctx, i)
 		require.NoError(t, err)
 		require.True(t, ib.Finalized)
+	}
+}
+
+func TestHandleResumeFinalityProposalWithJailedAndSlashedFp(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	bsKeeper := types.NewMockBTCStakingKeeper(ctrl)
+	iKeeper := types.NewMockIncentiveKeeper(ctrl)
+	// cKeeper := types.NewMockCheckpointingKeeper(ctrl)
+	fKeeper, ctx := keepertest.FinalityKeeper(t, bsKeeper, iKeeper, nil)
+
+	haltingHeight := uint64(100)
+	currentHeight := uint64(110)
+
+	// 4 FPs, after the missed blocks
+	// one will be slashed one jailed and two active
+	// each one with same VP, only one of the active is correctly voting
+	numFps := 4
+	activeFpPks := generateNFpPks(t, r, numFps)
+	setupActiveFps(t, activeFpPks, haltingHeight, fKeeper, ctx)
+	// set voting power table for each height, only the first fp votes
+	activeVotingFpPk, slashedFpPk, jailedFpPk, activeFpToBeJailed := activeFpPks[0], activeFpPks[1], activeFpPks[2], activeFpPks[3]
+	for h := haltingHeight; h <= currentHeight; h++ {
+		fKeeper.SetBlock(ctx, &types.IndexedBlock{
+			Height:    h,
+			AppHash:   datagen.GenRandomByteArray(r, 32),
+			Finalized: false,
+		})
+		dc := types.NewVotingPowerDistCache()
+		for i := 0; i < numFps; i++ {
+			fKeeper.SetVotingPower(ctx, activeFpPks[i].MustMarshal(), h, 1)
+			dc.AddFinalityProviderDistInfo(&types.FinalityProviderDistInfo{
+				BtcPk:          &activeFpPks[i],
+				TotalBondedSat: 1,
+				IsTimestamped:  true,
+			})
+		}
+		dc.ApplyActiveFinalityProviders(uint32(numFps))
+		votedSig, err := bbntypes.NewSchnorrEOTSSig(datagen.GenRandomByteArray(r, 32))
+		require.NoError(t, err)
+		fKeeper.SetSig(ctx, h, &activeVotingFpPk, votedSig)
+		fKeeper.SetVotingPowerDistCache(ctx, h, dc)
+	}
+
+	// tally blocks and none of them should be finalised
+	ctx = datagen.WithCtxHeight(ctx, currentHeight)
+	fKeeper.TallyBlocks(ctx, uint64(10000))
+	for i := haltingHeight; i < currentHeight; i++ {
+		ib, err := fKeeper.GetBlock(ctx, i)
+		require.NoError(t, err)
+		require.False(t, ib.Finalized)
+	}
+
+	slashedFp := &bstypes.FinalityProvider{
+		BtcPk:                &slashedFpPk,
+		Jailed:               false,
+		SlashedBabylonHeight: currentHeight,
+	}
+	bsKeeper.EXPECT().GetFinalityProvider(gomock.Any(), slashedFpPk.MustMarshal()).Return(slashedFp, nil).MaxTimes(1)
+
+	jailedFp := &bstypes.FinalityProvider{
+		BtcPk:  &slashedFpPk,
+		Jailed: true,
+	}
+	bsKeeper.EXPECT().GetFinalityProvider(gomock.Any(), jailedFpPk.MustMarshal()).Return(jailedFp, nil).MaxTimes(1)
+
+	activeNonVoting := &bstypes.FinalityProvider{
+		BtcPk:  &activeFpToBeJailed,
+		Jailed: false,
+	}
+	bsKeeper.EXPECT().GetFinalityProvider(gomock.Any(), activeFpToBeJailed.MustMarshal()).Return(activeNonVoting, nil).MaxTimes(1)
+
+	// create a resume finality proposal to jail the last fp only
+	// the already jailed and slashed fp should not be called
+	bsKeeper.EXPECT().JailFinalityProvider(ctx, activeFpToBeJailed.MustMarshal()).Return(nil).MaxTimes(1)
+	err := fKeeper.HandleResumeFinalityProposal(ctx, publicKeysToHex(activeFpPks[1:]), uint32(haltingHeight))
+	require.NoError(t, err)
+
+	fKeeper.TallyBlocks(ctx, types.MaxFinalizedRewardedBlocksPerEndBlock)
+
+	for i := haltingHeight; i <= currentHeight; i++ {
+		ib, err := fKeeper.GetBlock(ctx, i)
+		require.NoError(t, err)
+		require.True(t, ib.Finalized)
+	}
+
+	// check that the active non voting fp had the signing info updated
+	signingInfo, err := fKeeper.FinalityProviderSigningTracker.Get(ctx, activeFpToBeJailed)
+	require.NoError(t, err)
+	require.Zero(t, signingInfo.MissedBlocksCounter)
+
+	params := fKeeper.GetParams(ctx)
+	currentTime := ctx.HeaderInfo().Time
+	require.Equal(t, signingInfo.JailedUntil.String(), currentTime.Add(params.JailDuration).String())
+
+	// verifies the voting power distribution cache has set the FPs as jailed
+	// and the correct fps have zero voting power.
+	for h := haltingHeight; h <= currentHeight; h++ {
+		distCache := fKeeper.GetVotingPowerDistCache(ctx, h)
+
+		// checks the FPs have bonded sats, but zero voting power
+		for _, fpDstCache := range distCache.FinalityProviders {
+			vp := fKeeper.GetVotingPower(ctx, *fpDstCache.BtcPk, h)
+			require.Equal(t, fpDstCache.TotalBondedSat, uint64(1))
+
+			switch fpDstCache.BtcPk.MarshalHex() {
+			case activeVotingFpPk.MarshalHex():
+				require.Equal(t, vp, uint64(1))
+				require.False(t, fpDstCache.IsJailed)
+				require.False(t, fpDstCache.IsSlashed)
+			case slashedFpPk.MarshalHex():
+				require.Zero(t, vp)
+				// slashed could have been slashed for a few blocks before and
+				// it should keep as slashed, don't update to jailed
+				require.True(t, fpDstCache.IsSlashed || fpDstCache.IsJailed)
+			case jailedFpPk.MarshalHex():
+				require.Zero(t, vp)
+				require.True(t, fpDstCache.IsJailed)
+			case activeFpToBeJailed.MarshalHex():
+				require.Zero(t, vp)
+				require.True(t, fpDstCache.IsJailed)
+			default:
+				t.Error("unexpected fp")
+			}
+		}
 	}
 }
 
