9da1dfead1dfda6ab14f98817c30a0169b878efc
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-04-29 08:41:26 -0300

    backport(bugfix): add checks for slashed fp in gov resume finality (#829)  (#849)
    Prior to this, any slashed FP at the latest height wouldn't be jailed
    and the proposal would fail
    Add checks for FP slash and only set in vp distribution cache to jailed
    if it wasn't already slashed
    
    Also had to update replay driver code to run tests
    
    Backport: #829

---
+++ test/replay/covenant_sender.go
@@ -0,0 +1,155 @@
+package replay
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/babylonlabs-io/babylon/v2/btcstaking"
+	"github.com/babylonlabs-io/babylon/v2/types"
+	bstypes "github.com/babylonlabs-io/babylon/v2/x/btcstaking/types"
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcutil"
+
+	babylonApp "github.com/babylonlabs-io/babylon/v2/app"
+	"github.com/babylonlabs-io/babylon/v2/testutil/datagen"
+	"github.com/stretchr/testify/require"
+)
+
+type CovenantSender struct {
+	*SenderInfo
+	r   *rand.Rand
+	t   *testing.T
+	d   *BabylonAppDriver
+	app *babylonApp.BabylonApp
+}
+
+type StakingInfos struct {
+	FpPKs                 []*btcec.PublicKey
+	CovenantPks           []*btcec.PublicKey
+	StakerAddr            string
+	StakingTxHash         string
+	StakingSlashingInfo   *datagen.TestStakingSlashingInfo
+	UnbondingSlashingInfo *datagen.TestUnbondingSlashingInfo
+}
+
+func parseInfos(
+	t *testing.T,
+	resp *bstypes.BTCDelegationResponse,
+	params *bstypes.Params,
+) *StakingInfos {
+	stakingTx, _, err := types.NewBTCTxFromHex(resp.StakingTxHex)
+	require.NoError(t, err)
+	unbondingTx, _, err := types.NewBTCTxFromHex(resp.UndelegationResponse.UnbondingTxHex)
+	require.NoError(t, err)
+	slashingTx, err := bstypes.NewBTCSlashingTxFromHex(resp.SlashingTxHex)
+	require.NoError(t, err)
+	unbondingSlashingTx, err := bstypes.NewBTCSlashingTxFromHex(resp.UndelegationResponse.SlashingTxHex)
+	require.NoError(t, err)
+
+	fpPKs := make([]*btcec.PublicKey, len(resp.FpBtcPkList))
+	for i, pk := range resp.FpBtcPkList {
+		fpPKs[i] = pk.MustToBTCPK()
+	}
+
+	covenantPks := make([]*btcec.PublicKey, len(params.CovenantPks))
+
+	for i, pk := range params.CovenantPks {
+		covenantPks[i] = pk.MustToBTCPK()
+	}
+
+	stakingInfo, err := btcstaking.BuildStakingInfo(
+		resp.BtcPk.MustToBTCPK(),
+		fpPKs,
+		covenantPks,
+		params.CovenantQuorum,
+		uint16(resp.StakingTime),
+		btcutil.Amount(resp.TotalSat),
+		BtcParams,
+	)
+	require.NoError(t, err)
+
+	unbondingInfo, err := btcstaking.BuildUnbondingInfo(
+		resp.BtcPk.MustToBTCPK(),
+		fpPKs,
+		covenantPks,
+		params.CovenantQuorum,
+		uint16(resp.UnbondingTime),
+		btcutil.Amount(resp.TotalSat-uint64(params.UnbondingFeeSat)),
+		BtcParams,
+	)
+	require.NoError(t, err)
+
+	testStakingInfo := datagen.TestStakingSlashingInfo{
+		StakingTx:   stakingTx,
+		SlashingTx:  slashingTx,
+		StakingInfo: stakingInfo,
+	}
+
+	testUnbondingInfo := datagen.TestUnbondingSlashingInfo{
+		UnbondingTx:   unbondingTx,
+		SlashingTx:    unbondingSlashingTx,
+		UnbondingInfo: unbondingInfo,
+	}
+
+	stkTxHash := stakingTx.TxHash().String()
+
+	return &StakingInfos{
+		FpPKs:                 fpPKs,
+		CovenantPks:           covenantPks,
+		StakerAddr:            resp.StakerAddr,
+		StakingTxHash:         stkTxHash,
+		StakingSlashingInfo:   &testStakingInfo,
+		UnbondingSlashingInfo: &testUnbondingInfo,
+	}
+}
+
+func (c *CovenantSender) SendCovenantSignatures() {
+	pendingDels := c.d.GetPendingBTCDelegations(c.t)
+	params := c.d.GetBTCStakingParams(c.t)
+
+	for _, del := range pendingDels {
+		infos := parseInfos(c.t, del, params)
+
+		slashingPkScriptPath, err := infos.StakingSlashingInfo.StakingInfo.SlashingPathSpendInfo()
+		require.NoError(c.t, err)
+		unbondingPkScriptPath, err := infos.StakingSlashingInfo.StakingInfo.UnbondingPathSpendInfo()
+		require.NoError(c.t, err)
+
+		covenantSigs, err := datagen.GenCovenantAdaptorSigs(
+			covenantSKs,
+			infos.FpPKs,
+			infos.StakingSlashingInfo.StakingTx,
+			slashingPkScriptPath.GetPkScriptPath(),
+			infos.StakingSlashingInfo.SlashingTx,
+		)
+		require.NoError(c.t, err)
+
+		covUnbondingSlashingSigs, covUnbondingSigs, err := infos.UnbondingSlashingInfo.GenCovenantSigs(
+			covenantSKs,
+			infos.FpPKs,
+			infos.StakingSlashingInfo.StakingTx,
+			unbondingPkScriptPath.GetPkScriptPath(),
+		)
+		require.NoError(c.t, err)
+
+		for i := 0; i < len(infos.CovenantPks); i++ {
+			msgAddCovenantSig := &bstypes.MsgAddCovenantSigs{
+				Signer:                  c.AddressString(),
+				Pk:                      covenantSigs[i].CovPk,
+				StakingTxHash:           infos.StakingTxHash,
+				SlashingTxSigs:          covenantSigs[i].AdaptorSigs,
+				UnbondingTxSig:          covUnbondingSigs[i].Sig,
+				SlashingUnbondingTxSigs: covUnbondingSlashingSigs[i].AdaptorSigs,
+			}
+			// send each message with different transactions
+			DefaultSendTxWithMessagesSuccess(
+				c.t,
+				c.app,
+				c.SenderInfo,
+				msgAddCovenantSig,
+			)
+
+			c.IncSeq()
+		}
+	}
+}
---
+++ test/replay/driver_queries.go
@@ -0,0 +1,157 @@
+package replay
+
+import (
+	goMath "math"
+	"testing"
+
+	btckckpttypes "github.com/babylonlabs-io/babylon/v2/x/btccheckpoint/types"
+	"github.com/btcsuite/btcd/wire"
+	govk "github.com/cosmos/cosmos-sdk/x/gov/keeper"
+	govv1types "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+
+	ftypes "github.com/babylonlabs-io/babylon/v2/x/finality/types"
+
+	bstypes "github.com/babylonlabs-io/babylon/v2/x/btcstaking/types"
+	ckpttypes "github.com/babylonlabs-io/babylon/v2/x/checkpointing/types"
+	et "github.com/babylonlabs-io/babylon/v2/x/epoching/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/stretchr/testify/require"
+)
+
+func (d *BabylonAppDriver) getDelegationWithStatus(t *testing.T, status bstypes.BTCDelegationStatus) []*bstypes.BTCDelegationResponse {
+	pagination := &query.PageRequest{}
+	pagination.Limit = goMath.MaxUint32
+
+	ctx, err := d.App.CreateQueryContext(0, false)
+	require.NoError(t, err)
+	delegations, err := d.App.BTCStakingKeeper.BTCDelegations(ctx, &bstypes.QueryBTCDelegationsRequest{
+		Status:     status,
+		Pagination: pagination,
+	})
+	require.NoError(t, err)
+	return delegations.BtcDelegations
+}
+
+func (d *BabylonAppDriver) GetAllBTCDelegations(t *testing.T) []*bstypes.BTCDelegationResponse {
+	return d.getDelegationWithStatus(t, bstypes.BTCDelegationStatus_ANY)
+}
+
+func (d *BabylonAppDriver) GetVerifiedBTCDelegations(t *testing.T) []*bstypes.BTCDelegationResponse {
+	return d.getDelegationWithStatus(t, bstypes.BTCDelegationStatus_VERIFIED)
+}
+
+func (d *BabylonAppDriver) GetActiveBTCDelegations(t *testing.T) []*bstypes.BTCDelegationResponse {
+	return d.getDelegationWithStatus(t, bstypes.BTCDelegationStatus_ACTIVE)
+}
+
+func (d *BabylonAppDriver) GetPendingBTCDelegations(t *testing.T) []*bstypes.BTCDelegationResponse {
+	return d.getDelegationWithStatus(t, bstypes.BTCDelegationStatus_PENDING)
+}
+
+func (d *BabylonAppDriver) GetBTCStakingParams(t *testing.T) *bstypes.Params {
+	params := d.App.BTCStakingKeeper.GetParams(d.GetContextForLastFinalizedBlock())
+	return &params
+}
+
+func (d *BabylonAppDriver) GetEpochingParams() et.Params {
+	return d.App.EpochingKeeper.GetParams(d.GetContextForLastFinalizedBlock())
+}
+
+func (d *BabylonAppDriver) GetEpoch() *et.Epoch {
+	return d.App.EpochingKeeper.GetEpoch(d.GetContextForLastFinalizedBlock())
+}
+
+func (d *BabylonAppDriver) GetCheckpoint(
+	t *testing.T,
+	epochNumber uint64,
+) *ckpttypes.RawCheckpointWithMeta {
+	checkpoint, err := d.App.CheckpointingKeeper.GetRawCheckpoint(d.GetContextForLastFinalizedBlock(), epochNumber)
+	require.NoError(t, err)
+	return checkpoint
+}
+
+func (d *BabylonAppDriver) GetLastFinalizedEpoch() uint64 {
+	return d.App.CheckpointingKeeper.GetLastFinalizedEpoch(d.GetContextForLastFinalizedBlock())
+}
+
+func (d *BabylonAppDriver) GetActiveFpsAtHeight(t *testing.T, height uint64) []*ftypes.ActiveFinalityProvidersAtHeightResponse {
+	res, err := d.App.FinalityKeeper.ActiveFinalityProvidersAtHeight(
+		d.GetContextForLastFinalizedBlock(),
+		&ftypes.QueryActiveFinalityProvidersAtHeightRequest{
+			Height:     height,
+			Pagination: &query.PageRequest{},
+		},
+	)
+	require.NoError(t, err)
+	return res.FinalityProviders
+}
+
+func (d *BabylonAppDriver) GetVotingPowerDistCache(height uint64) *ftypes.VotingPowerDistCache {
+	return d.App.FinalityKeeper.GetVotingPowerDistCache(d.Ctx(), height)
+}
+
+func (d *BabylonAppDriver) GovProposals() []*govv1types.Proposal {
+	resp, err := d.GovQuerySvr().Proposals(d.Ctx(), &govv1types.QueryProposalsRequest{
+		ProposalStatus: govv1types.ProposalStatus_PROPOSAL_STATUS_VOTING_PERIOD,
+	})
+	require.NoError(d.t, err)
+	return resp.Proposals
+}
+
+func (d *BabylonAppDriver) GovProposal(propId uint64) *govv1types.Proposal {
+	resp, err := d.GovQuerySvr().Proposal(d.Ctx(), &govv1types.QueryProposalRequest{
+		ProposalId: propId,
+	})
+	require.NoError(d.t, err)
+	return resp.Proposal
+}
+
+func (d *BabylonAppDriver) GovQuerySvr() govv1types.QueryServer {
+	return govk.NewQueryServer(&d.App.GovKeeper)
+}
+
+func (d *BabylonAppDriver) GetAllFps(t *testing.T) []*bstypes.FinalityProviderResponse {
+	res, err := d.App.BTCStakingKeeper.FinalityProviders(
+		d.GetContextForLastFinalizedBlock(),
+		&bstypes.QueryFinalityProvidersRequest{},
+	)
+	require.NoError(t, err)
+	return res.FinalityProviders
+}
+
+func (d *BabylonAppDriver) GetActiveFpsAtCurrentHeight(t *testing.T) []*ftypes.ActiveFinalityProvidersAtHeightResponse {
+	return d.GetActiveFpsAtHeight(t, d.GetLastFinalizedBlock().Height)
+}
+
+func (d *BabylonAppDriver) GetFp(fpBTCPK []byte) *bstypes.FinalityProvider {
+	fp, err := d.App.BTCStakingKeeper.GetFinalityProvider(d.GetContextForLastFinalizedBlock(), fpBTCPK)
+	require.NoError(d.t, err)
+	return fp
+}
+
+func (d *BabylonAppDriver) GetActivationHeight(t *testing.T) uint64 {
+	res, err := d.App.FinalityKeeper.ActivatedHeight(
+		d.GetContextForLastFinalizedBlock(),
+		&ftypes.QueryActivatedHeightRequest{},
+	)
+	require.NoError(t, err)
+	return res.Height
+}
+
+func (d *BabylonAppDriver) GetIndexedBlock(height uint64) *ftypes.IndexedBlock {
+	res, err := d.App.FinalityKeeper.Block(
+		d.GetContextForLastFinalizedBlock(),
+		&ftypes.QueryBlockRequest{Height: height},
+	)
+	require.NoError(d.t, err)
+	return res.Block
+}
+
+func (d *BabylonAppDriver) GetBTCCkptParams(t *testing.T) btckckpttypes.Params {
+	return d.App.BtcCheckpointKeeper.GetParams(d.GetContextForLastFinalizedBlock())
+}
+
+func (d *BabylonAppDriver) GetBTCLCTip() (*wire.BlockHeader, uint32) {
+	tipInfo := d.App.BTCLightClientKeeper.GetTipInfo(d.GetContextForLastFinalizedBlock())
+	return tipInfo.Header.ToBlockHeader(), tipInfo.Height
+}
