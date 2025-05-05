c079ac005550188202c759ec7ee134af07569119
KonradStaniec <konrad.staniec@gmail.com>
2025-05-05 12:08:01 +0200

    [release/v1.1.x backport] add v1.1 upgrade data (#857) (#873)
    - Adds upgrade data for v1.1

--- app/include_upgrade_mainnet.go
+++ app/include_upgrade_mainnet.go
@@ -6,11 +6,12 @@ import (
 	"github.com/babylonlabs-io/babylon/app/upgrades"
 	v1 "github.com/babylonlabs-io/babylon/app/upgrades/v1"
 	"github.com/babylonlabs-io/babylon/app/upgrades/v1/mainnet"
+	v1_1 "github.com/babylonlabs-io/babylon/app/upgrades/v1_1"
 )
 
 // init is used to include v1 upgrade for mainnet data
 func init() {
-	Upgrades = []upgrades.Upgrade{v1.CreateUpgrade(v1.UpgradeDataString{
+	Upgrades = []upgrades.Upgrade{v1_1.Upgrade, v1.CreateUpgrade(v1.UpgradeDataString{
 		BtcStakingParamsStr:       mainnet.BtcStakingParamsStr,
 		FinalityParamStr:          mainnet.FinalityParamStr,
 		IncentiveParamStr:         mainnet.IncentiveParamStr,
--- app/include_upgrade_testnet.go
+++ app/include_upgrade_testnet.go
@@ -6,6 +6,7 @@ import (
 	"github.com/babylonlabs-io/babylon/app/upgrades"
 	v1 "github.com/babylonlabs-io/babylon/app/upgrades/v1"
 	"github.com/babylonlabs-io/babylon/app/upgrades/v1/testnet"
+	v1_1 "github.com/babylonlabs-io/babylon/app/upgrades/v1_1"
 	v1rc5 "github.com/babylonlabs-io/babylon/app/upgrades/v1rc5/testnet"
 	v1rc8 "github.com/babylonlabs-io/babylon/app/upgrades/v1rc8/testnet"
 	v1rc9 "github.com/babylonlabs-io/babylon/app/upgrades/v1rc9/testnet"
@@ -15,6 +16,7 @@ import (
 // it is also used for e2e testing
 func init() {
 	Upgrades = []upgrades.Upgrade{
+		v1_1.Upgrade,
 		v1.CreateUpgrade(v1.UpgradeDataString{
 			BtcStakingParamsStr:       testnet.BtcStakingParamsStr,
 			FinalityParamStr:          testnet.FinalityParamStr,
