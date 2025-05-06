e6bdbc1a35f863e1884cb7c1b35e6445f0c07a0d
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-05-06 16:44:22 -0300

    chore: add test for delete reward tracker and add

--- x/incentive/keeper/reward_tracker_events_store_test.go
+++ x/incentive/keeper/reward_tracker_events_store_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func FuzzGetOrNewRewardTrackerEvent(f *testing.F) {
+func FuzzSetGetOrNewRewardTrackerEvent(f *testing.F) {
 	datagen.AddRandomSeedsToFuzzer(f, 10)
 
 	f.Fuzz(func(t *testing.T, seed int64) {
@@ -37,5 +37,55 @@ func FuzzGetOrNewRewardTrackerEvent(f *testing.F) {
 		require.NoError(t, err)
 		require.Len(t, new.Events, 2)
 		require.EqualValues(t, old, new)
+	})
+}
+
+func FuzzAddRewardTrackerEventAndDeletes(f *testing.F) {
+	datagen.AddRandomSeedsToFuzzer(f, 10)
+
+	f.Fuzz(func(t *testing.T, seed int64) {
+		t.Parallel()
+		r := rand.New(rand.NewSource(seed))
+
+		k, ctx := NewKeeperWithCtx(t)
+		fp1, fp2, del1 := datagen.GenRandomAddress(), datagen.GenRandomAddress(), datagen.GenRandomAddress()
+
+		blkHeight := datagen.RandomInt(r, 1000) + 2
+
+		err := k.AddEventBtcDelegationActivated(ctx, blkHeight, fp1, del1, datagen.RandomInt(r, 1000)+100)
+		require.NoError(t, err)
+		err = k.AddEventBtcDelegationActivated(ctx, blkHeight, fp2, del1, datagen.RandomInt(r, 1000)+100)
+		require.NoError(t, err)
+		// different height
+		nextBlockHeight := blkHeight + 1 + datagen.RandomInt(r, 100)
+		err = k.AddEventBtcDelegationUnbonded(ctx, nextBlockHeight, fp2, del1, datagen.RandomInt(r, 98)+1)
+		require.NoError(t, err)
+
+		new, err := k.GetOrNewRewardTrackerEvent(ctx, blkHeight)
+		require.NoError(t, err)
+		require.Len(t, new.Events, 2)
+
+		newNext, err := k.GetOrNewRewardTrackerEvent(ctx, nextBlockHeight)
+		require.NoError(t, err)
+		require.Len(t, newNext.Events, 1)
+
+		// call delete twice for same height
+		err = k.DeleteRewardTrackerEvents(ctx, blkHeight)
+		require.NoError(t, err)
+		err = k.DeleteRewardTrackerEvents(ctx, blkHeight)
+		require.NoError(t, err)
+
+		// check if there is no reward tracker there
+		new, err = k.GetOrNewRewardTrackerEvent(ctx, blkHeight)
+		require.NoError(t, err)
+		require.Len(t, new.Events, 0)
+
+		// check next
+		err = k.DeleteRewardTrackerEvents(ctx, nextBlockHeight)
+		require.NoError(t, err)
+
+		next, err := k.GetOrNewRewardTrackerEvent(ctx, nextBlockHeight)
+		require.NoError(t, err)
+		require.Len(t, next.Events, 0)
 	})
 }
