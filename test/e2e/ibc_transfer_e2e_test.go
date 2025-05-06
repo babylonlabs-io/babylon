b69e658506df8bc685905da648c909c5d189b686
tom <tomasguerraalda@hotmail.com>
2025-05-06 11:39:36 -0300

    fix lint

--- test/e2e/ibc_transfer_e2e_test.go
+++ test/e2e/ibc_transfer_e2e_test.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 	"time"
 
-	"cosmossdk.io/math"
 	"github.com/babylonlabs-io/babylon/v2/test/e2e/configurer"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	pfmroutertypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8/packetforward/types"
