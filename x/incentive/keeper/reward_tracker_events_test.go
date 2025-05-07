c73089473444fb5a74b7def42f9bbe6608be1889
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-05-07 13:47:16 -0300

    chore: add test for ProcessRewardTrackerEvents

--- x/incentive/keeper/reward_tracker_events_test.go
+++ x/incentive/keeper/reward_tracker_events_test.go
@@ -9,6 +9,73 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func FuzzProcessRewardTrackerEventsAtHeight(f *testing.F) {
+	datagen.AddRandomSeedsToFuzzer(f, 10)
+
+	f.Fuzz(func(t *testing.T, seed int64) {
+		t.Parallel()
+		r := rand.New(rand.NewSource(seed))
+
+		k, ctx := NewKeeperWithCtx(t)
+		fp1, fp2, del1 := datagen.GenRandomAddress(), datagen.GenRandomAddress(), datagen.GenRandomAddress()
+		blkHeight := datagen.RandomInt(r, 1000) + 2
+
+		lastProcessedHeight, err := k.GetRewardTrackerEventLastProcessedHeight(ctx)
+		require.NoError(t, err)
+		require.EqualValues(t, lastProcessedHeight, 0)
+
+		err = k.ProcessRewardTrackerEventsAtHeight(ctx, blkHeight)
+		require.NoError(t, err)
+
+		rAmtSat, rAmtSat2 := datagen.RandomInt(r, 1000)+1, datagen.RandomInt(r, 2000)+2
+
+		err = k.AddEventBtcDelegationActivated(ctx, blkHeight, fp1, del1, rAmtSat)
+		require.NoError(t, err)
+		err = k.AddEventBtcDelegationActivated(ctx, blkHeight, fp2, del1, rAmtSat2)
+		require.NoError(t, err)
+
+		nextBlkHeight := blkHeight + 1 + datagen.RandomInt(r, 1000)
+		subAmtSat2 := rAmtSat2 / 2
+
+		err = k.AddEventBtcDelegationUnbonded(ctx, nextBlkHeight, fp2, del1, subAmtSat2)
+		require.NoError(t, err)
+
+		err = k.ProcessRewardTrackerEvents(ctx, nextBlkHeight)
+		require.NoError(t, err)
+		// call twice should not error out
+		err = k.ProcessRewardTrackerEvents(ctx, nextBlkHeight)
+		require.NoError(t, err)
+
+		// check if the events were modified
+		evts, err := k.GetOrNewRewardTrackerEvent(ctx, blkHeight)
+		require.NoError(t, err)
+		require.Len(t, evts.Events, 0)
+		evtsNextHeight, err := k.GetOrNewRewardTrackerEvent(ctx, nextBlkHeight)
+		require.NoError(t, err)
+		require.Len(t, evtsNextHeight.Events, 0)
+
+		lastProcessedHeight, err = k.GetRewardTrackerEventLastProcessedHeight(ctx)
+		require.NoError(t, err)
+		require.EqualValues(t, lastProcessedHeight, nextBlkHeight)
+
+		// check if the amounts match in the reward tracker
+		fp1Current, err := k.GetFinalityProviderCurrentRewards(ctx, fp1)
+		require.NoError(t, err)
+		require.Equal(t, fp1Current.TotalActiveSat.Uint64(), rAmtSat)
+
+		fp2Current, err := k.GetFinalityProviderCurrentRewards(ctx, fp2)
+		require.NoError(t, err)
+		require.Equal(t, fp2Current.TotalActiveSat.Uint64(), rAmtSat2-subAmtSat2)
+
+		delFp1, err := k.GetBTCDelegationRewardsTracker(ctx, fp1, del1)
+		require.NoError(t, err)
+		require.Equal(t, delFp1.TotalActiveSat.Uint64(), rAmtSat)
+		delFp2, err := k.GetBTCDelegationRewardsTracker(ctx, fp2, del1)
+		require.NoError(t, err)
+		require.Equal(t, delFp2.TotalActiveSat.Uint64(), rAmtSat2-subAmtSat2)
+	})
+}
+
 func FuzzSetGetOrNewRewardTrackerEvent(f *testing.F) {
 	datagen.AddRandomSeedsToFuzzer(f, 10)
 
