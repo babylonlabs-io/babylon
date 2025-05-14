c61b337c2adb1723b855fd0b38b07f25f290c39b
Vlad <vladislav.varadinov@gmail.com>
2025-05-14 11:02:49 +0300

    fix: update tokenfactory params to use ubbn

--- app/upgrades/v2/upgrades.go
+++ app/upgrades/v2/upgrades.go
@@ -2,10 +2,12 @@ package v2
 
 import (
 	"context"
-
+	"cosmossdk.io/math"
 	store "cosmossdk.io/store/types"
 	"github.com/babylonlabs-io/babylon/v2/app/keepers"
 	"github.com/babylonlabs-io/babylon/v2/app/upgrades"
+	minttypes "github.com/babylonlabs-io/babylon/v2/x/mint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	pfmroutertypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8/packetforward/types"
 	tokenfactorytypes "github.com/strangelove-ventures/tokenfactory/x/tokenfactory/types"
 
@@ -30,6 +32,13 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, 
 		// Run migrations before applying any other state changes.
 		migrations, err := mm.RunMigrations(ctx, configurator, fromVM)
 		if err != nil {
+			return nil, err
+		}
+
+		// Set the denom creation fee to ubbn
+		params := keepers.TokenFactoryKeeper.GetParams(ctx)
+		params.DenomCreationFee = sdk.NewCoins(sdk.NewCoin(minttypes.DefaultBondDenom, math.NewInt(10000000)))
+		if err := keepers.TokenFactoryKeeper.SetParams(ctx, params); err != nil {
 			return nil, err
 		}
 
