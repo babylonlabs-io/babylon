e7e0e1d6123a9e786e4e2e24ddf4aefac72308f8
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2025-05-14 11:28:56 +0200

    chore: add bank wrapper for distribution mod account. (#906)

--- app/params/addr.go
+++ app/params/addr.go
@@ -2,10 +2,12 @@ package params
 
 import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	dstrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 var (
 	AccGov          = authtypes.NewModuleAddress(govtypes.ModuleName)
+	AccDistribution = authtypes.NewModuleAddress(dstrtypes.ModuleName)
 	AccFeeCollector = authtypes.NewModuleAddress(authtypes.FeeCollectorName)
 )
