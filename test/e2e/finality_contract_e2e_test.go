63f193ab85458b274af54e717caf28c36ddfef89
Mauro Lacy <maurolacy@users.noreply.github.com>
2025-05-27 10:24:04 +0200

    F/op e2e 2 (#946)
    Second step (register consumer chain) of finality contract e2e tests. #945 follow-up

--- test/e2e/finality_contract_e2e_test.go
+++ test/e2e/finality_contract_e2e_test.go
@@ -1,19 +1,35 @@
 package e2e
 
 import (
+	"github.com/stretchr/testify/require"
+	"math/rand"
 	"strconv"
 	"time"
 
 	"github.com/stretchr/testify/suite"
 
+	bsctypes "github.com/babylonlabs-io/babylon/v4/x/btcstkconsumer/types"
+
 	"github.com/babylonlabs-io/babylon/v4/test/e2e/configurer"
 	"github.com/babylonlabs-io/babylon/v4/test/e2e/initialization"
+	"github.com/babylonlabs-io/babylon/v4/testutil/datagen"
+)
+
+const (
+	ConsumerID = "optimism-1234"
+)
+
+var (
+	r = rand.New(rand.NewSource(time.Now().Unix()))
 )
 
 type FinalityContractTestSuite struct {
 	suite.Suite
 
 	configurer configurer.Configurer
+
+	// Cross-test config data
+	FinalityContractAddr string
 }
 
 func (s *FinalityContractTestSuite) SetupSuite() {
@@ -72,7 +88,7 @@ func (s *FinalityContractTestSuite) Test1InstantiateFinalityContract() {
 		strconv.Itoa(latestWasmId),
 		`{
 			"admin": "`+adminAddr+`",
-			"consumer_id": "optimism-1234",
+			"consumer_id": "`+ConsumerID+`",
 			"is_enabled": true
 		}`,
 		initialization.ValidatorWalletName,
@@ -83,6 +99,45 @@ func (s *FinalityContractTestSuite) Test1InstantiateFinalityContract() {
 		contracts, err = nonValidatorNode.QueryContractsFromId(latestWasmId)
 		return err == nil && len(contracts) == 1
 	}, time.Second*10, time.Second)
-	contractAddr := contracts[0]
-	s.T().Log("Finality gadget contract address: ", contractAddr)
+	s.FinalityContractAddr = contracts[0]
+	s.T().Log("Finality gadget contract address: ", s.FinalityContractAddr)
+}
+
+func (s *FinalityContractTestSuite) Test2RegisterRollupConsumer() {
+	var registeredConsumer *bsctypes.ConsumerRegister
+	var err error
+
+	// Register the consumer id on Babylon
+	registeredConsumer = bsctypes.NewCosmosConsumerRegister(
+		ConsumerID,
+		datagen.GenRandomHexStr(r, 5),
+		"Chain description: "+datagen.GenRandomHexStr(r, 15),
+		3,
+	)
+
+	validatorNode, err := s.configurer.GetChainConfig(0).GetNodeAtIndex(0)
+	require.NoError(s.T(), err)
+
+	// TODO: Register the Consumer through a gov proposal
+	validatorNode.RegisterRollupConsumerChain(initialization.ValidatorWalletName, registeredConsumer.ConsumerId, registeredConsumer.ConsumerName, registeredConsumer.ConsumerDescription, s.FinalityContractAddr)
+
+	nonValidatorNode, err := s.configurer.GetChainConfig(0).GetNodeAtIndex(2)
+	require.NoError(s.T(), err)
+
+	// Confirm the consumer is registered
+	s.Eventually(func() bool {
+		consumerRegistryResp := nonValidatorNode.QueryBTCStkConsumerConsumer(ConsumerID)
+		s.Require().NotNil(consumerRegistryResp)
+		s.Require().Len(consumerRegistryResp.ConsumerRegisters, 1)
+		s.Require().Equal(registeredConsumer.ConsumerId, consumerRegistryResp.ConsumerRegisters[0].ConsumerId)
+		s.Require().Equal(registeredConsumer.ConsumerName, consumerRegistryResp.ConsumerRegisters[0].ConsumerName)
+		s.Require().Equal(registeredConsumer.ConsumerDescription, consumerRegistryResp.ConsumerRegisters[0].ConsumerDescription)
+
+		return true
+	}, 10*time.Second, 2*time.Second, "Consumer was not registered within the expected time")
+
+	s.T().Logf("Consumer registered: ID=%s, Name=%s, Description=%s",
+		registeredConsumer.ConsumerId,
+		registeredConsumer.ConsumerName,
+		registeredConsumer.ConsumerDescription)
 }
