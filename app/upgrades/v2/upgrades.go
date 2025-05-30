ad98bcebc5421db452430dd0211372f2ad6b7588
Vlad <vladislav.varadinov@gmail.com>
2025-05-30 11:04:32 +0300

    fix: add missing denom

--- app/upgrades/v2/upgrades.go
+++ app/upgrades/v2/upgrades.go
@@ -11,7 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 
 	pfmroutertypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v10/packetforward/types"
-	//icqtypes "github.com/cosmos/ibc-apps/modules/async-icq/v8/types"
+
 	ratelimitkeeper "github.com/cosmos/ibc-apps/modules/rate-limiting/v10/keeper"
 	ratelimittypes "github.com/cosmos/ibc-apps/modules/rate-limiting/v10/types"
 	icacontrollertypes "github.com/cosmos/ibc-go/v10/modules/apps/27-interchain-accounts/controller/types"
@@ -39,8 +39,7 @@ var (
 		UpgradeName:          UpgradeName,
 		CreateUpgradeHandler: CreateUpgradeHandler,
 		StoreUpgrades: store.StoreUpgrades{
-			Added: []string{tokenfactorytypes.ModuleName, pfmroutertypes.StoreKey, icacontrollertypes.StoreKey, icahosttypes.StoreKey, ratelimittypes.StoreKey}, // icqtypes.StoreKey,},
-
+			Added:   []string{tokenfactorytypes.ModuleName, pfmroutertypes.StoreKey, icacontrollertypes.StoreKey, icahosttypes.StoreKey, ratelimittypes.StoreKey},
 			Deleted: []string{},
 		},
 	}
@@ -111,10 +110,11 @@ func addRateLimits(ctx sdk.Context, chk transfertypes.ChannelKeeper, rlk ratelim
 func addRateLimit(ctx sdk.Context, k ratelimitkeeper.Keeper, denom, channel string, percent sdkmath.Int, durationHours uint64) error {
 	addRateLimitMsg := ratelimittypes.MsgAddRateLimit{
 		// TODO: Add missing params
-		Denom:          denom,
-		MaxPercentSend: percent,
-		MaxPercentRecv: percent,
-		DurationHours:  durationHours,
+		Denom:             denom,
+		ChannelOrClientId: channel,
+		MaxPercentSend:    percent,
+		MaxPercentRecv:    percent,
+		DurationHours:     durationHours,
 	}
 
 	err := k.AddRateLimit(ctx, &addRateLimitMsg)
