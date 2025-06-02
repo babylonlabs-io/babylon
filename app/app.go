54051564274ef8b220c8e198e33a70a18ec8e507
Vlad <vladislav.varadinov@gmail.com>
2025-06-02 11:14:22 +0300

    fix: linter

--- app/app.go
+++ app/app.go
@@ -281,8 +281,8 @@ func NewBabylonApp(
 	clientKeeper.AddRoute(ibctm.ModuleName, &tmLightClientModule)
 
 	// TODO: Do we we need this ?
-	//wasmLightClientModule := ibcwasm.NewLightClientModule(app.WasmClientKeeper, clientKeeper.GetStoreProvider())
-	//clientKeeper.AddRoute(ibcwasmtypes.ModuleName, &wasmLightClientModule)
+	// wasmLightClientModule := ibcwasm.NewLightClientModule(app.WasmClientKeeper, clientKeeper.GetStoreProvider())
+	// clientKeeper.AddRoute(ibcwasmtypes.ModuleName, &wasmLightClientModule)
 
 	/****  Module Options ****/
 
