bf39dc18350047e32010cb0735df950994f4f507
Gurjot Singh <111540954+gusin13@users.noreply.github.com>
2025-06-04 12:33:02 -0400

    chore(btcstkconsumer): rename ETH L2 to rollup  (#1040)
    ETH L2 was the old terminology, we should use `rollup` to be consistent

--- x/btcstkconsumer/types/errors.go
+++ x/btcstkconsumer/types/errors.go
@@ -13,7 +13,7 @@ var (
 	ErrConsumerAlreadyRegistered    = sdkerrors.Register(ModuleName, 1102, "consumer already registered")
 	ErrInvalidConsumerIDs           = sdkerrors.Register(ModuleName, 1103, "consumer ids contain duplicates or empty strings")
 	ErrInvalidCosmosConsumerRequest = sdkerrors.Register(ModuleName, 1104, "invalid registration request of Cosmos consumer")
-	ErrInvalidETHL2ConsumerRequest  = sdkerrors.Register(ModuleName, 1105, "invalid registration request of ETH L2 consumer")
+	ErrInvalidRollupConsumerRequest = sdkerrors.Register(ModuleName, 1105, "invalid registration request of rollup consumer")
 	ErrInvalidMaxMultiStakedFps     = sdkerrors.Register(ModuleName, 1106, "max multi staked fps must be at least 2 to allow for at least one Babylon FP and one consumer FP")
 	ErrEmptyConsumerId              = sdkerrors.Register(ModuleName, 1107, "consumer id must be non-empty")
 	ErrEmptyConsumerName            = sdkerrors.Register(ModuleName, 1108, "consumer name must be non-empty")
