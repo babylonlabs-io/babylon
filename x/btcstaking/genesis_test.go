080fe220ccb4b18d78840aab3ed31cb1d06e82cc
tom <tomasguerraalda@hotmail.com>
2025-06-05 18:21:28 -0300

    fix test setup

--- x/btcstaking/genesis_test.go
+++ x/btcstaking/genesis_test.go
@@ -8,9 +8,12 @@ import (
 	storemetrics "cosmossdk.io/store/metrics"
 	keepertest "github.com/babylonlabs-io/babylon/v4/testutil/keeper"
 	"github.com/babylonlabs-io/babylon/v4/testutil/nullify"
+	btcctypes "github.com/babylonlabs-io/babylon/v4/x/btccheckpoint/types"
 	"github.com/babylonlabs-io/babylon/v4/x/btcstaking"
 	"github.com/babylonlabs-io/babylon/v4/x/btcstaking/types"
 	dbm "github.com/cosmos/cosmos-db"
+
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,7 +24,12 @@ func TestGenesis(t *testing.T) {
 	}
 	db := dbm.NewMemDB()
 	stateStore := store.NewCommitMultiStore(db, log.NewTestLogger(t), storemetrics.NewNoOpMetrics())
-	k, ctx := keepertest.BTCStakingKeeperWithStore(t, db, stateStore, nil, nil, nil, nil)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	btccKeeper := types.NewMockBtcCheckpointKeeper(ctrl)
+
+	btccKeeper.EXPECT().GetParams(gomock.Any()).AnyTimes().Return(btcctypes.DefaultParams())
+	k, ctx := keepertest.BTCStakingKeeperWithStore(t, db, stateStore, nil, nil, btccKeeper, nil)
 
 	btcstaking.InitGenesis(ctx, *k, genesisState)
 	got := btcstaking.ExportGenesis(ctx, *k)
