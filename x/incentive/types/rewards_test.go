bc4ed64562b8520b2e79764df8bb437cd11911d7
Lazar <lukic.lazar@yahoo.com>
2025-06-05 21:55:03 +0200

    fix import

--- x/incentive/types/rewards_test.go
+++ x/incentive/types/rewards_test.go
@@ -2,8 +2,8 @@ package types_test
 
 import (
 	"cosmossdk.io/math"
-	"github.com/babylonlabs-io/babylon/v4/testutil/datagen"
-	"github.com/babylonlabs-io/babylon/v4/x/incentive/types"
+	"github.com/babylonlabs-io/babylon/v3/testutil/datagen"
+	"github.com/babylonlabs-io/babylon/v3/x/incentive/types"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
