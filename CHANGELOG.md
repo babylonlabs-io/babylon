e7155ea5f5014c52f011879e0833710f4db6777e
Lazar <12626340+Lazar955@users.noreply.github.com>
2025-06-05 21:49:06 +0200

    chore: validate block diff (#1089)
    Ref issue no. 22 of the audit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -99,6 +99,7 @@ check of rewards
 - [#1085](https://github.com/babylonlabs-io/babylon/pull/1085) Update comment in `SetRewardTrackerEvent` function
 - [#1097](https://github.com/babylonlabs-io/babylon/pull/1097) Update `Evidence.ValidateBasic` function
 - [#1096](https://github.com/babylonlabs-io/babylon/pull/1096) chore: validate stats positive
+- [#1089](https://github.com/babylonlabs-io/babylon/pull/1089) chore: validate block diff
 
 ### State Machine Breaking
 
--- x/btcstaking/genesis.go
+++ x/btcstaking/genesis.go
@@ -2,6 +2,7 @@ package btcstaking
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/babylonlabs-io/babylon/v4/x/btcstaking/keeper"
 	"github.com/babylonlabs-io/babylon/v4/x/btcstaking/types"
@@ -12,6 +13,15 @@ func InitGenesis(ctx context.Context, k keeper.Keeper, gs types.GenesisState) {
 	if err := gs.Validate(); err != nil {
 		panic(err)
 	}
+
+	p := k.BtccKeeper().GetParams(ctx)
+	btcConfirmationDepth := p.BtcConfirmationDepth
+
+	if gs.LargestBtcReorg != nil && gs.LargestBtcReorg.BlockDiff >= btcConfirmationDepth {
+		panic(fmt.Sprintf("genesis LargestBtcReOrg block_diff %d must be less than btc_confirmation_depth %d to prevent immediate chain halt",
+			gs.LargestBtcReorg.BlockDiff, btcConfirmationDepth))
+	}
+
 	if err := k.InitGenesis(ctx, gs); err != nil {
 		panic(err)
 	}
