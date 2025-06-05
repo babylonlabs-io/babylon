fe5e648b9c1a0a98f9962b7739ebac1b8c04e046
Vlad <vladislav.varadinov@gmail.com>
2025-06-05 16:37:35 +0300

    fix: v2

--- x/finality/types/signing_info_test.go
+++ x/finality/types/signing_info_test.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	bbntypes "github.com/babylonlabs-io/babylon/v4/types"
-	"github.com/babylonlabs-io/babylon/v4/x/finality/types"
+	bbntypes "github.com/babylonlabs-io/babylon/v2/types"
+	"github.com/babylonlabs-io/babylon/v2/x/finality/types"
 )
 
 func TestFinalityProviderSigningInfo_Validate(t *testing.T) {
