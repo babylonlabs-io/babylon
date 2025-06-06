c5bce22ffc06886fe6036837da98f7a5c76a42cf
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2025-06-06 07:33:52 +0200

    Apply suggestions from code review
    Co-authored-by: RafilxTenfen <rafaeltenfen.rt@gmail.com>

--- x/incentive/types/rewards.go
+++ x/incentive/types/rewards.go
@@ -98,9 +98,8 @@ func (f *FinalityProviderCurrentRewards) Validate() error {
 		return fmt.Errorf("current rewards has a negative total active satoshi delegated value: %s", f.TotalActiveSat.String())
 	}
 
-	//nolint:staticcheck
-	if f.Period < 0 {
-		return fmt.Errorf("period cannot be negative")
+	if f.Period == 0 {
+		return fmt.Errorf("fp current rewards period must be positive")
 	}
 
 	return nil
