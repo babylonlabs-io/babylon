e1ec868348361585d9c5d33863c2c435c946113c
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-06-07 09:20:40 -0300

    fix: conflicts

--- app/include_upgrade_testnet.go
+++ app/include_upgrade_testnet.go
@@ -3,7 +3,6 @@
 package app
 
 import (
-<<<<<<< HEAD
 	"github.com/babylonlabs-io/babylon/v2/app/upgrades"
 	v1 "github.com/babylonlabs-io/babylon/v2/app/upgrades/v1"
 	"github.com/babylonlabs-io/babylon/v2/app/upgrades/v1/testnet"
@@ -12,18 +11,7 @@ import (
 	v1rc8 "github.com/babylonlabs-io/babylon/v2/app/upgrades/v1rc8/testnet"
 	v1rc9 "github.com/babylonlabs-io/babylon/v2/app/upgrades/v1rc9/testnet"
 	v2 "github.com/babylonlabs-io/babylon/v2/app/upgrades/v2"
-	v2rc2 "github.com/babylonlabs-io/babylon/v2/app/upgrades/v2rc2/testnet"
-=======
-	"github.com/babylonlabs-io/babylon/v4/app/upgrades"
-	v1 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v1"
-	"github.com/babylonlabs-io/babylon/v4/app/upgrades/v1/testnet"
-	v1_1 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v1_1"
-	v1rc5 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v1rc5/testnet"
-	v1rc8 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v1rc8/testnet"
-	v1rc9 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v1rc9/testnet"
-	v2 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2"
-	v2rc3 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2rc3/testnet"
->>>>>>> e57af39 (chore: rename upgrade to v2rc3 (#1181))
+	v2rc3 "github.com/babylonlabs-io/babylon/v2/app/upgrades/v2rc3/testnet"
 )
 
 // init is used to include v1 upgrade testnet data
