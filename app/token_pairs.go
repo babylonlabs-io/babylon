3a2d0f037e216b8a6084e118ad1e3989c8c5ca3f
Vlad <vladislav.varadinov@gmail.com>
2025-06-12 14:56:03 +0300

    fix: add comment for tokenpairs

--- app/token_pairs.go
+++ app/token_pairs.go
@@ -8,8 +8,9 @@ import (
 // WTokenContractMainnet is the WrappedToken contract address for mainnet
 const WTokenContractMainnet = "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517"
 
-// TokenPairs creates a slice of token pairs, that contains a pair for the native denom of the example chain
-// implementation.
+// TokenPairs creates a slice of token pairs, that contains a pair for the native denom of the Babylon
+// chain and it's corresponding ERC20 address.
+// TokenPairs represents all of the Coins that have a corresponding ERC20 precompile.
 var TokenPairs = []erc20types.TokenPair{
 	{
 		Erc20Address:  WTokenContractMainnet,
