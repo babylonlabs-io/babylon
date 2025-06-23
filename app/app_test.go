2aeeb04d5d0b9b1ec2d2fe5dbd762eeed4711c71
KonradStaniec <konrad.staniec@gmail.com>
2025-06-23 10:36:46 +0200

    Backport v2.1 fix to staking endblocker (#1249)
    Co-authored-by: RafilxTenfen <rafaeltenfen.rt@gmail.com>
    (cherry picked from commit 41a286133f984b8abf9e2d824fce006cf6c97b08)
    
    # Conflicts:
    #	app/app_test.go

--- app/app_test.go
+++ app/app_test.go
@@ -2,7 +2,18 @@ package app_test
 
 import (
 	"fmt"
+<<<<<<< HEAD
 	"testing"
+=======
+	"strings"
+	"testing"
+
+	appparams "github.com/babylonlabs-io/babylon/v3/app/params"
+	erc20types "github.com/cosmos/evm/x/erc20/types"
+	feemarkettypes "github.com/cosmos/evm/x/feemarket/types"
+	precisebanktypes "github.com/cosmos/evm/x/precisebank/types"
+	evmtypes "github.com/cosmos/evm/x/vm/types"
+>>>>>>> 41a2861 (Backport v2.1 fix to staking endblocker (#1249))
 
 	tokenfactorytypes "github.com/strangelove-ventures/tokenfactory/x/tokenfactory/types"
 
@@ -41,6 +52,32 @@ var (
 		icatypes.ModuleName:          nil,
 	}
 )
+
+func TestBabylonCheckRemovedStakingEndBlocker(t *testing.T) {
+	db := dbm.NewMemDB()
+
+	tbs, err := testsigner.SetupTestBlsSigner()
+	require.NoError(t, err)
+	blsSigner := checkpointingtypes.BlsSigner(tbs)
+
+	logger := log.NewTestLogger(t)
+	appOpts, cleanup := babylonApp.TmpAppOptions()
+	defer cleanup()
+
+	app := babylonApp.NewBabylonAppWithCustomOptions(t, false, blsSigner, babylonApp.SetupOptions{
+		Logger:             logger,
+		DB:                 db,
+		InvCheckPeriod:     0,
+		SkipUpgradeHeights: map[int64]bool{},
+		AppOpts:            appOpts,
+	})
+
+	for _, m := range app.ModuleManager.OrderEndBlockers {
+		if strings.EqualFold(m, stktypes.ModuleName) {
+			t.Error("the staking module is active to execute end blocker")
+		}
+	}
+}
 
 func TestBabylonBlockedAddrs(t *testing.T) {
 	db := dbm.NewMemDB()
