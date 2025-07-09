b315c71423c14851755a609af4a50945af1bc6d6
Vlad <vladislav.varadinov@gmail.com>
2025-07-09 17:12:21 +0300

    fix: add a log for the fork event

--- x/zoneconcierge/keeper/header_handler.go
+++ x/zoneconcierge/keeper/header_handler.go
@@ -44,7 +44,15 @@ func (k Keeper) HandleHeaderWithValidCommit(ctx context.Context, txHash []byte, 
 		}
 	}
 
-	if !isOnFork {
+	if isOnFork {
+		// Log the fork event
+		k.Logger(sdkCtx).Info(
+			"fork detected",
+			"consumer_id", indexedHeader.ConsumerId,
+			"height", indexedHeader.Height,
+			"babylon_height", indexedHeader.BabylonHeaderHeight,
+		)
+	} else {
 		// ensure the header is the latest one, otherwise ignore it
 		// NOTE: while an old header is considered acceptable in IBC-Go (see Case_valid_past_update), but
 		// ZoneConcierge should not checkpoint it since Babylon requires monotonic checkpointing
