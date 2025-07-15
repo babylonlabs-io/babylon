cd7b00519d90e300d52409b274d279c8884b55f7
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-07-15 10:40:27 -0300

    chore: add babylon commission to ConsumerRegistry (#1364)
    This PR adds a new field to the consumer registry and related structures
    (Response, Events and Msg) that defines the babylon commission per BSNs,
    currently this field is not being used but it will after #1359
    
    (cherry picked from commit e26cafc6180141f1342121ac3444c039a55e0d0d)
    
    # Conflicts:
    #	client/docs/swagger-ui/swagger.yaml

--- testutil/cli/cmd.go
+++ testutil/cli/cmd.go
@@ -2,11 +2,78 @@ package cli
 
 import (
 	"context"
+	"io"
+	"testing"
 
+	abci "github.com/cometbft/cometbft/abci/types"
+	cmtbytes "github.com/cometbft/cometbft/libs/bytes"
+	rpcclient "github.com/cometbft/cometbft/rpc/client"
+	rpcclientmock "github.com/cometbft/cometbft/rpc/client/mock"
+	coretypes "github.com/cometbft/cometbft/rpc/core/types"
+	cmttypes "github.com/cometbft/cometbft/types"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/cosmos/cosmos-sdk/testutil"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+
+	"github.com/babylonlabs-io/babylon/v3/app"
 )
+
+type mockCometRPC struct {
+	rpcclientmock.Client
+	responseQuery abci.ResponseQuery
+}
+
+func newMockCometRPC(respQuery abci.ResponseQuery) mockCometRPC {
+	return mockCometRPC{responseQuery: respQuery}
+}
+
+func (mockCometRPC) BroadcastTxSync(_ context.Context, _ cmttypes.Tx) (*coretypes.ResultBroadcastTx, error) {
+	return &coretypes.ResultBroadcastTx{}, nil
+}
+
+func (m mockCometRPC) ABCIQueryWithOptions(
+	_ context.Context,
+	_ string, _ cmtbytes.HexBytes,
+	_ rpcclient.ABCIQueryOptions,
+) (*coretypes.ResultABCIQuery, error) {
+	return &coretypes.ResultABCIQuery{Response: m.responseQuery}, nil
+}
+
+func SetupClientContext(t *testing.T) (client.Context, []sdk.AccAddress) {
+	encCfg := app.GetEncodingConfig()
+	kr := keyring.NewInMemory(encCfg.Codec)
+
+	baseCtx := client.Context{}.
+		WithKeyring(kr).
+		WithTxConfig(encCfg.TxConfig).
+		WithCodec(encCfg.Codec).
+		WithClient(mockCometRPC{Client: rpcclientmock.Client{}}).
+		WithAccountRetriever(client.MockAccountRetriever{}).
+		WithOutput(io.Discard).
+		WithChainID("test-chain")
+
+	bz, _ := encCfg.Codec.Marshal(&sdk.TxResponse{})
+	c := newMockCometRPC(abci.ResponseQuery{Value: bz})
+	clientCtx := baseCtx.WithClient(c)
+
+	addrs := make([]sdk.AccAddress, 0)
+	for i := 0; i < 3; i++ {
+		k, _, err := clientCtx.Keyring.NewMnemonic("NewValidator", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+		require.NoError(t, err)
+
+		pub, err := k.GetPubKey()
+		require.NoError(t, err)
+
+		newAddr := sdk.AccAddress(pub.Address())
+		addrs = append(addrs, newAddr)
+	}
+
+	return clientCtx, addrs
+}
 
 // ExecTestCLICmd builds the client context, mocks the output and executes the command.
 func ExecTestCLICmd(clientCtx client.Context, cmd *cobra.Command, extraArgs []string) (testutil.BufferWriter, error) {
--- x/btcstkconsumer/types/consumer_register_test.go
+++ x/btcstkconsumer/types/consumer_register_test.go
@@ -1,8 +1,10 @@
 package types_test
 
 import (
+	"fmt"
 	"testing"
 
+	"cosmossdk.io/math"
 	"github.com/babylonlabs-io/babylon/v3/x/btcstkconsumer/types"
 	"github.com/test-go/testify/require"
 )
@@ -58,6 +60,128 @@ func TestConsumerRegisterValidate(t *testing.T) {
 			} else {
 				require.Error(t, err)
 				require.EqualError(t, err, tc.expectedErr)
+			}
+		})
+	}
+}
+
+func TestMsgRegisterConsumerValidateBasic(t *testing.T) {
+	validCommission := math.LegacyMustNewDecFromStr("0.5")
+
+	testCases := []struct {
+		name     string
+		msg      *types.MsgRegisterConsumer
+		expected error
+	}{
+		{
+			name: "valid message",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   validCommission,
+			},
+			expected: nil,
+		},
+		{
+			name: "empty consumer ID",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   validCommission,
+			},
+			expected: fmt.Errorf("ConsumerId must be non-empty"),
+		},
+		{
+			name: "empty consumer name",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   validCommission,
+			},
+			expected: fmt.Errorf("ConsumerName must be non-empty"),
+		},
+		{
+			name: "empty consumer description",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "",
+				BabylonRewardsCommission:   validCommission,
+			},
+			expected: fmt.Errorf("ConsumerDescription must be non-empty"),
+		},
+		{
+			name: "negative babylon commission",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   math.LegacyMustNewDecFromStr("-0.1"),
+			},
+			expected: fmt.Errorf("babylon commission cannot be negative"),
+		},
+		{
+			name: "babylon commission greater than 1.0",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   math.LegacyMustNewDecFromStr("1.5"),
+			},
+			expected: fmt.Errorf("babylon commission cannot be greater than 1.0"),
+		},
+		{
+			name: "babylon commission exactly 0.0",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   math.LegacyZeroDec(),
+			},
+			expected: nil,
+		},
+		{
+			name: "babylon commission exactly 1.0",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   math.LegacyOneDec(),
+			},
+			expected: nil,
+		},
+		{
+			name: "babylon commission with high precision",
+			msg: &types.MsgRegisterConsumer{
+				Signer:              "babylon1validaddress",
+				ConsumerId:          "consumer-123",
+				ConsumerName:        "Test Consumer",
+				ConsumerDescription: "Test Description",
+				BabylonRewardsCommission:   math.LegacyNewDecWithPrec(123456789012345678, 18),
+			},
+			expected: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.msg.ValidateBasic()
+			if tc.expected != nil {
+				require.Error(t, err)
+				require.Equal(t, tc.expected.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
 			}
 		})
 	}
