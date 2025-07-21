84765a5ca8f240fcdd14ea0794d0883c43c4dd94
Sam Ricotta <samanthalricotta@gmail.com>
2025-07-21 14:37:40 +0300

    wip softwareUpgradeE2Ev3Test

--- test/e2e/software_upgrade_e2e_v3_test.go
+++ test/e2e/software_upgrade_e2e_v3_test.go
@@ -1,12 +1,11 @@
 package e2e
 
 import (
-	"github.com/babylonlabs-io/babylon/v3/x/mint/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
 
 	appparams "github.com/babylonlabs-io/babylon/v3/app/params"
-	v2 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v2"
+	v3 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v3"
 	"github.com/babylonlabs-io/babylon/v3/testutil/sample"
 	btclighttypes "github.com/babylonlabs-io/babylon/v3/x/btclightclient/types"
 
@@ -16,18 +15,18 @@ import (
 )
 
 const (
-	TokenFactoryModulePath = "tokenfactory"
+	btcstkconsumerModulePath = "btcstkconsumer"
 )
 
-type SoftwareUpgradeV2TestSuite struct {
+type SoftwareUpgradeV3TestSuite struct {
 	suite.Suite
 
 	configurer            *configurer.UpgradeConfigurer
 	balancesBeforeUpgrade map[string]sdk.Coin
 }
 
-func (s *SoftwareUpgradeV2TestSuite) SetupSuite() {
-	s.T().Log("setting up e2e integration test suite for v1.1 to v2 upgrade...")
+func (s *SoftwareUpgradeV3TestSuite) SetupSuite() {
+	s.T().Log("setting up e2e integration test suite for v2.2.0 to v3 upgrade...")
 	var err error
 	s.balancesBeforeUpgrade = make(map[string]sdk.Coin)
 
@@ -53,7 +52,7 @@ func (s *SoftwareUpgradeV2TestSuite) SetupSuite() {
 	cfg, err := configurer.NewSoftwareUpgradeConfigurer(
 		s.T(),
 		true,
-		config.UpgradeV2FilePath,
+		config.UpgradeV3FilePath,
 		[]*btclighttypes.BTCHeaderInfo{btcHeaderGenesis},
 		preUpgradeFunc,
 	)
@@ -66,16 +65,15 @@ func (s *SoftwareUpgradeV2TestSuite) SetupSuite() {
 	s.Require().NoError(err)
 }
 
-func (s *SoftwareUpgradeV2TestSuite) TearDownSuite() {
+func (s *SoftwareUpgradeV3TestSuite) TearDownSuite() {
 	err := s.configurer.ClearResources()
 	if err != nil {
 		s.T().Logf("error to clear resources %s", err.Error())
 	}
 }
 
-// TestUpgradeV2 checks if the upgrade from v1.1 to v2 was successful
-func (s *SoftwareUpgradeV2TestSuite) TestUpgradeV2() {
-	// Chain is already upgraded, check for new modules and state changes
+// TestUpgradeV3 checks if the upgrade from v2.2.0 to v3 was successful
+func (s *SoftwareUpgradeV3TestSuite) TestUpgradeV3() {
 	chainA := s.configurer.GetChainConfig(0)
 
 	n, err := chainA.GetNodeAtIndex(1)
@@ -86,27 +84,20 @@ func (s *SoftwareUpgradeV2TestSuite) TestUpgradeV2() {
 	chainA.WaitUntilHeight(govProp.Plan.Height + 1) // waits for chain to produce blocks
 
 	expectedUpgradeHeight := govProp.Plan.Height
-	resp := n.QueryAppliedPlan(v2.UpgradeName)
+	resp := n.QueryAppliedPlan(v3.UpgradeName)
 	s.EqualValues(expectedUpgradeHeight, resp.Height, "the plan should be applied at the height %d", expectedUpgradeHeight)
 
-	// Check that the module exists by querying parameters with the QueryParams helper
-	var tokenfactoryParams map[string]interface{}
-	n.QueryParams(TokenFactoryModulePath, &tokenfactoryParams)
-	s.T().Logf("Tokenfactory params: %v", tokenfactoryParams)
+	// check that the module exists by querying parameters with the QueryParams helper
+	var btcstkconsumerParams map[string]interface{}
+	n.QueryParams(btcstkconsumerModulePath, &btcstkconsumerParams)
+	s.T().Logf("btcstkconsumer params: %v", btcstkconsumerParams)
 
-	params, ok := tokenfactoryParams["params"].(map[string]interface{})
+	btcstkparams, ok := btcstkconsumerParams["params"].(map[string]interface{})
 	s.Require().True(ok, "params field should exist and be a map")
 
-	denomCreationFee, ok := params["denom_creation_fee"].([]interface{})
-	s.Require().True(ok, "denom_creation_fee should be a list")
-	s.Require().Len(denomCreationFee, 1, "denom_creation_fee should have one entry")
-
-	feeEntry, ok := denomCreationFee[0].(map[string]interface{})
-	s.Require().True(ok, "fee entry should be a map")
-	s.Equal(types.DefaultBondDenom, feeEntry["denom"])
-	s.Equal("10000000", feeEntry["amount"])
-
-	s.Equal("2000000", params["denom_creation_gas_consume"])
+	permissionedIntegration, ok := btcstkparams["permissioned_integration"].([]interface{})
+	s.Require().True(ok, "permissioned_integration field should exist and be a map")
+	s.Equal(permissionedIntegration, true)
 
 	n.WaitForNextBlock()
 
