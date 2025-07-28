21a98ae8fa47ecf24f67a24920434235138ab374
tom <tomasguerraalda@hotmail.com>
2025-07-28 10:41:01 -0300

    fix: update stk exp events

---
+++ testutil/events/utils.go
@@ -0,0 +1,20 @@
+package events
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/test-go/testify/require"
+)
+
+func RequireEventAttribute(t *testing.T, event sdk.Event, key, expectedValue string, msgAndArgs ...any) {
+	t.Helper()
+	for _, attr := range event.Attributes {
+		if string(attr.Key) == key {
+			require.Equal(t, expectedValue, string(attr.Value), msgAndArgs...)
+			return
+		}
+	}
+	require.Fail(t, "Expected attribute not found", msgAndArgs...)
+}
