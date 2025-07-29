46bbd5cff801ff5395fdcb6d2e813480eb0648af
samricotta <37125168+samricotta@users.noreply.github.com>
2025-07-29 18:46:12 +0300

    upgrade: update maxFps and btcActivatioHeight in btcStakingParams for V3 (#1432)
    Updates values for V3s upgrade handler.
    
    Following consensus we decided to push the maxFps to 15 on testnet and 5
    on main. The values for the activation need to be confirmed once again
    during the reviewal
    
    (cherry picked from commit 954b1c2a8929002830fef3a8839f2e78c83416eb)

--- app/upgrades/v3/upgrades_test.go
+++ app/upgrades/v3/upgrades_test.go
@@ -1,6 +1,9 @@
 package v3
 
 import (
+	"testing"
+	"time"
+
 	"cosmossdk.io/core/appmodule"
 	"cosmossdk.io/core/header"
 	"cosmossdk.io/x/upgrade"
@@ -11,8 +14,6 @@ import (
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
-	"testing"
-	"time"
 )
 
 const (
@@ -21,24 +22,32 @@ const (
 
 type UpgradeTestSuite struct {
 	suite.Suite
-	ctx       sdk.Context
-	app       *app.BabylonApp
-	preModule appmodule.HasPreBlocker
+	ctx              sdk.Context
+	app              *app.BabylonApp
+	preModule        appmodule.HasPreBlocker
+	initialBtcHeight uint64
 }
 
-func TestUpdateTestSuite(t *testing.T) {
+func TestUpgradeTestSuite(t *testing.T) {
 	suite.Run(t, new(UpgradeTestSuite))
 }
 
-func (s *UpgradeTestSuite) SetupTest() {
-	app.Upgrades = []upgrades.Upgrade{CreateUpgrade()}
+func (s *UpgradeTestSuite) setupTestWithNetwork(fpCount uint32, btcActivationHeight uint32) {
+	s.initialBtcHeight = 100
+
+	app.Upgrades = []upgrades.Upgrade{CreateUpgrade(fpCount, btcActivationHeight)}
 
 	s.app = app.SetupWithBitcoinConf(s.T(), false, bbn.BtcSignet)
 	s.ctx = s.app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "babylon-1", Time: time.Now().UTC()})
 	s.preModule = upgrade.NewAppModule(s.app.UpgradeKeeper, s.app.AccountKeeper.AddressCodec())
+
+	params := s.app.BTCStakingKeeper.GetParams(s.ctx)
+	params.BtcActivationHeight = uint32(s.initialBtcHeight)
+	err := s.app.BTCStakingKeeper.SetParams(s.ctx, params)
+	s.Require().NoError(err)
 }
 
-func (s *UpgradeTestSuite) Upgrade() {
+func (s *UpgradeTestSuite) executeUpgrade() {
 	s.ctx = s.ctx.WithBlockHeight(DummyUpgradeHeight - 1)
 	plan := upgradetypes.Plan{Name: UpgradeName, Height: DummyUpgradeHeight}
 	err := s.app.UpgradeKeeper.ScheduleUpgrade(s.ctx, plan)
@@ -57,25 +66,46 @@ func (s *UpgradeTestSuite) Upgrade() {
 	})
 }
 
-func (s *UpgradeTestSuite) TestUpgrade() {
-	s.SetupTest()
-	s.PreUpgrade()
-	s.Upgrade()
-	s.PostUpgrade()
+func (s *UpgradeTestSuite) TestUpgradeNetworks() {
+	testCases := []struct {
+		name                      string
+		expectedMaxFPs            uint32
+		expectedBtcActivationHeight uint32
+	}{
+		{
+			name:                        "mainnet upgrade",
+			expectedMaxFPs:              5,
+			expectedBtcActivationHeight: 915000,
+		},
+		{
+			name:                        "testnet upgrade", 
+			expectedMaxFPs:              10,
+			expectedBtcActivationHeight: 260000,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			s.setupTestWithNetwork(tc.expectedMaxFPs, tc.expectedBtcActivationHeight)
+
+			s.executeUpgrade()
+
+			s.verifyPostUpgrade(tc.expectedMaxFPs, tc.expectedBtcActivationHeight)
+		})
+	}
 }
 
-func (s *UpgradeTestSuite) PreUpgrade() {}
-
-func (s *UpgradeTestSuite) PostUpgrade() {
+func (s *UpgradeTestSuite) verifyPostUpgrade(expectedMaxFPs, expectedBtcActivationHeight uint32) {
 	_, found := s.app.ModuleManager.Modules[deletedCapabilityStoreKey]
-	s.Require().False(found, "x/capability module shouldn't be found")
+	s.Require().False(found, "x/capability module should be deleted")
 
 	_, found = s.app.ModuleManager.Modules["btcstkconsumer"]
-	s.Require().True(found, "x/btcstkconsumer module shouldn't be found")
+	s.Require().True(found, "x/btcstkconsumer module should be found")
 
 	_, found = s.app.ModuleManager.Modules["zoneconcierge"]
-	s.Require().True(found, "x/zoneconcierge module shouldn't be found")
+	s.Require().True(found, "x/zoneconcierge module should be found")
 
 	params := s.app.BTCStakingKeeper.GetParams(s.ctx)
-	s.Require().Equal(uint32(1), params.MaxFinalityProviders)
+	s.Require().Equal(expectedMaxFPs, params.MaxFinalityProviders, "MaxFinalityProviders should match expected value")
+	s.Require().Equal(expectedBtcActivationHeight, params.BtcActivationHeight, "BtcActivationHeight should be set to absolute height")
 }
