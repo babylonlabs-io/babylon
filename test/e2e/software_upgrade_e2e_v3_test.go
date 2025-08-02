c08621781ee47b50f9dd7064430f8ce8e31a059d
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-08-02 18:27:57 -0300

    chore: refactor and removed skip of upgrade v3

--- test/e2e/software_upgrade_e2e_v3_test.go
+++ test/e2e/software_upgrade_e2e_v3_test.go
@@ -5,14 +5,10 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec/v2"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
 
-	appparams "github.com/babylonlabs-io/babylon/v3/app/params"
 	v3 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v3"
 	"github.com/babylonlabs-io/babylon/v3/testutil/datagen"
-	"github.com/babylonlabs-io/babylon/v3/testutil/sample"
-	btclighttypes "github.com/babylonlabs-io/babylon/v3/x/btclightclient/types"
 	bstypes "github.com/babylonlabs-io/babylon/v3/x/btcstaking/types"
 	bsconstypes "github.com/babylonlabs-io/babylon/v3/x/btcstkconsumer/types"
 	"github.com/babylonlabs-io/babylon/v3/x/finality/types"
@@ -25,100 +21,39 @@ import (
 type SoftwareUpgradeV3TestSuite struct {
 	suite.Suite
 
-	configurer            *configurer.UpgradeConfigurer
-	balancesBeforeUpgrade map[string]sdk.Coin
-	fp1Addr               string
-	fp2Addr               string
-	r                     *rand.Rand
-	fp1BTCSK              *btcec.PrivateKey
-	fp2BTCSK              *btcec.PrivateKey
-	fp1                   *bstypes.FinalityProvider
-	fp2                   *bstypes.FinalityProvider
-	fp1CommitPubRandList  *types.MsgCommitPubRandList
-	fp2CommitPubRandList  *types.MsgCommitPubRandList
-	commitStartHeight     uint64
+	r          *rand.Rand
+	configurer *configurer.UpgradeConfigurer
+
+	fp1Addr              string
+	fp2Addr              string
+	fp1BTCSK             *btcec.PrivateKey
+	fp2BTCSK             *btcec.PrivateKey
+	fp1                  *bstypes.FinalityProvider
+	fp2                  *bstypes.FinalityProvider
+	fp1CommitPubRandList *types.MsgCommitPubRandList
+	fp2CommitPubRandList *types.MsgCommitPubRandList
+	commitStartHeight    uint64
 }
 
 func (s *SoftwareUpgradeV3TestSuite) SetupSuite() {
-	s.T().Skip("Temporarily skipping v3 upgrade test")
 	s.T().Log("setting up e2e integration test suite for v2.2.0 to v3 upgrade...")
+	s.r = rand.New(rand.NewSource(time.Now().UnixNano()))
 	var err error
-	s.balancesBeforeUpgrade = make(map[string]sdk.Coin)
-
-	btcHeaderGenesis := sample.SignetBtcHeader195552(s.T())
 
 	// func runs right before the upgrade proposal is sent
 	preUpgradeFunc := func(chains []*chain.Config) {
 		n := chains[0].NodeConfigs[1]
-
-		s.fp1Addr = n.KeysAdd("fp1Addr")
-		s.fp2Addr = n.KeysAdd("fp2Addr")
-		n.BankMultiSendFromNode([]string{s.fp1Addr, s.fp2Addr}, "900000ubbn")
-
-		n.WaitForNextBlock()
-
-		_, err := n.QueryBalance(s.fp1Addr, appparams.DefaultBondDenom)
-		s.NoError(err)
-		_, err = n.QueryBalance(s.fp2Addr, appparams.DefaultBondDenom)
-		s.NoError(err)
-
-		s.r = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-		s.fp1BTCSK, _, _ = datagen.GenRandomBTCKeyPair(s.r)
-		s.fp2BTCSK, _, _ = datagen.GenRandomBTCKeyPair(s.r)
-
-		s.fp1 = CreateNodeFPV2(
-			s.T(),
-			s.r,
-			s.fp1BTCSK,
-			n,
-			s.fp1Addr,
-		)
-
-		s.fp2 = CreateNodeFPV2(
-			s.T(),
-			s.r,
-			s.fp2BTCSK,
-			n,
-			s.fp2Addr,
-		)
-
-		n.WaitForNextBlock()
-
-		s.commitStartHeight = n.LatestBlockNumber()
-		_, s.fp1CommitPubRandList, err = datagen.GenRandomMsgCommitPubRandList(s.r, s.fp1BTCSK, "", s.commitStartHeight, numPubRand)
-		s.NoError(err)
-		_, s.fp2CommitPubRandList, err = datagen.GenRandomMsgCommitPubRandList(s.r, s.fp2BTCSK, "", s.commitStartHeight, numPubRand)
-		s.NoError(err)
-
-		s.Require().NotNil(s.fp1CommitPubRandList, "fp1CommitPubRandList should not be nil")
-		s.Require().NotNil(s.fp2CommitPubRandList, "fp2CommitPubRandList should not be nil")
-
-		n.CommitPubRandList(
-			s.fp1CommitPubRandList.FpBtcPk,
-			s.fp1CommitPubRandList.StartHeight,
-			s.fp1CommitPubRandList.NumPubRand,
-			s.fp1CommitPubRandList.Commitment,
-			s.fp1CommitPubRandList.Sig,
-		)
-		n.CommitPubRandList(
-			s.fp2CommitPubRandList.FpBtcPk,
-			s.fp2CommitPubRandList.StartHeight,
-			s.fp2CommitPubRandList.NumPubRand,
-			s.fp2CommitPubRandList.Commitment,
-			s.fp2CommitPubRandList.Sig,
-		)
+		s.SetupFps(n)
 	}
 
-	cfg, err := configurer.NewSoftwareUpgradeConfigurer(
+	s.configurer, err = configurer.NewSoftwareUpgradeConfigurer(
 		s.T(),
 		true,
 		config.UpgradeV3FilePath,
-		[]*btclighttypes.BTCHeaderInfo{btcHeaderGenesis},
+		nil,
 		preUpgradeFunc,
 	)
 	s.NoError(err)
-	s.configurer = cfg
 
 	err = s.configurer.ConfigureChains()
 	s.NoError(err)
@@ -131,6 +66,60 @@ func (s *SoftwareUpgradeV3TestSuite) TearDownSuite() {
 	if err != nil {
 		s.T().Logf("error to clear resources %s", err.Error())
 	}
+}
+
+func (s *SoftwareUpgradeV3TestSuite) SetupFps(n *chain.NodeConfig) {
+	s.fp1Addr = n.KeysAdd("fp1Addr")
+	s.fp2Addr = n.KeysAdd("fp2Addr")
+	n.BankMultiSendFromNode([]string{s.fp1Addr, s.fp2Addr}, "900000ubbn")
+
+	n.WaitForNextBlock()
+
+	s.fp1BTCSK, _, _ = datagen.GenRandomBTCKeyPair(s.r)
+	s.fp2BTCSK, _, _ = datagen.GenRandomBTCKeyPair(s.r)
+
+	s.fp1 = CreateNodeFPV2(
+		s.T(),
+		s.r,
+		s.fp1BTCSK,
+		n,
+		s.fp1Addr,
+	)
+
+	s.fp2 = CreateNodeFPV2(
+		s.T(),
+		s.r,
+		s.fp2BTCSK,
+		n,
+		s.fp2Addr,
+	)
+
+	n.WaitForNextBlock()
+	s.commitStartHeight = n.LatestBlockNumber()
+
+	var err error
+	_, s.fp1CommitPubRandList, err = datagen.GenRandomMsgCommitPubRandList(s.r, s.fp1BTCSK, "", s.commitStartHeight, numPubRand)
+	s.NoError(err)
+	_, s.fp2CommitPubRandList, err = datagen.GenRandomMsgCommitPubRandList(s.r, s.fp2BTCSK, "", s.commitStartHeight, numPubRand)
+	s.NoError(err)
+
+	s.Require().NotNil(s.fp1CommitPubRandList, "fp1CommitPubRandList should not be nil")
+	s.Require().NotNil(s.fp2CommitPubRandList, "fp2CommitPubRandList should not be nil")
+
+	n.CommitPubRandList(
+		s.fp1CommitPubRandList.FpBtcPk,
+		s.fp1CommitPubRandList.StartHeight,
+		s.fp1CommitPubRandList.NumPubRand,
+		s.fp1CommitPubRandList.Commitment,
+		s.fp1CommitPubRandList.Sig,
+	)
+	n.CommitPubRandList(
+		s.fp2CommitPubRandList.FpBtcPk,
+		s.fp2CommitPubRandList.StartHeight,
+		s.fp2CommitPubRandList.NumPubRand,
+		s.fp2CommitPubRandList.Commitment,
+		s.fp2CommitPubRandList.Sig,
+	)
 }
 
 // TestUpgradeV3 checks if the upgrade from v2.2.0 to v3 was successful
