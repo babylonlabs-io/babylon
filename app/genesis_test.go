682a2954c83d151ddfa11372808b8d370d23e7a2
Chris <chancold123@gmail.com>
2025-08-06 14:25:21 +0900

    chore: change app test package name

--- app/genesis_test.go
+++ app/genesis_test.go
@@ -1,4 +1,4 @@
-package app
+package app_test
 
 import (
 	"testing"
@@ -6,30 +6,32 @@ import (
 	"github.com/stretchr/testify/require"
 
 	erc20types "github.com/cosmos/evm/x/erc20/types"
+
+	"github.com/babylonlabs-io/babylon/v3/app"
 )
 
 // BabylonApp DefaultGenesis ERC20 NativePrecompiles test
 func TestBabylonApp_DefaultGenesis_ERC20NativePrecompiles(t *testing.T) {
 	// Test that BabylonApp.DefaultGenesis() correctly sets NativePrecompiles
-	app := NewTmpBabylonApp()
-	genesis := app.DefaultGenesis()
+	tmpBabylonApp := app.NewTmpBabylonApp()
+	genesis := tmpBabylonApp.DefaultGenesis()
 
 	// Verify ERC20 module genesis is present
 	require.Contains(t, genesis, erc20types.ModuleName, "ERC20 module should be in genesis")
 
 	// Unmarshal ERC20 genesis state
 	var erc20GenState erc20types.GenesisState
-	err := app.appCodec.UnmarshalJSON(genesis[erc20types.ModuleName], &erc20GenState)
+	err := tmpBabylonApp.AppCodec().UnmarshalJSON(genesis[erc20types.ModuleName], &erc20GenState)
 	require.NoError(t, err, "Should be able to unmarshal ERC20 genesis state")
 
 	// Verify NativePrecompiles contains WTokenContractMainnet
-	require.Contains(t, erc20GenState.NativePrecompiles, WTokenContractMainnet,
+	require.Contains(t, erc20GenState.NativePrecompiles, app.WTokenContractMainnet,
 		"NativePrecompiles should contain WTokenContractMainnet")
 	require.Len(t, erc20GenState.NativePrecompiles, 1,
 		"Should have exactly one native precompile")
 
 	// Verify it's the correct contract address
-	require.Equal(t, WTokenContractMainnet, erc20GenState.NativePrecompiles[0],
+	require.Equal(t, app.WTokenContractMainnet, erc20GenState.NativePrecompiles[0],
 		"First native precompile should be WTokenContractMainnet")
 
 	// Verify the field structure (not nested under Params)
