08b458bbe97e5aa1612253a5fb36c64516b56876
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-08-08 14:34:02 -0300

    chore: allow zero len coins as fp historical rewards (#1529)
    (cherry picked from commit 25a0cd3b2e1ded75e061bb0656f674aaaa27ef66)
    
    # Conflicts:
    #	CHANGELOG.md
    #	x/incentive/genesis_test.go

--- x/incentive/genesis_test.go
+++ x/incentive/genesis_test.go
@@ -3,10 +3,19 @@ package incentive_test
 import (
 	"testing"
 
+<<<<<<< HEAD
 	keepertest "github.com/babylonlabs-io/babylon/v2/testutil/keeper"
 	"github.com/babylonlabs-io/babylon/v2/testutil/nullify"
 	"github.com/babylonlabs-io/babylon/v2/x/incentive"
 	"github.com/babylonlabs-io/babylon/v2/x/incentive/types"
+=======
+	sdkmath "cosmossdk.io/math"
+	"github.com/babylonlabs-io/babylon/v3/testutil/datagen"
+	keepertest "github.com/babylonlabs-io/babylon/v3/testutil/keeper"
+	"github.com/babylonlabs-io/babylon/v3/testutil/nullify"
+	"github.com/babylonlabs-io/babylon/v3/x/incentive"
+	"github.com/babylonlabs-io/babylon/v3/x/incentive/types"
+>>>>>>> 25a0cd3 (chore: allow zero len coins as fp historical rewards (#1529))
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,6 +24,17 @@ func TestGenesis(t *testing.T) {
 
 	k, ctx := keepertest.IncentiveKeeper(t, nil, nil, nil)
 	incentive.InitGenesis(ctx, *k, *genesisState)
+	fp1, fp2, del1, del2 := datagen.GenRandomAddress(), datagen.GenRandomAddress(), datagen.GenRandomAddress(), datagen.GenRandomAddress()
+
+	// creates empty historical data
+	amt := uint64(30)
+	err := k.BtcDelegationActivated(ctx, fp1, del1, sdkmath.NewIntFromUint64(amt))
+	require.NoError(t, err)
+	err = k.BtcDelegationActivated(ctx, fp2, del2, sdkmath.NewIntFromUint64(amt))
+	require.NoError(t, err)
+	err = k.BtcDelegationUnbonded(ctx, fp2, del2, sdkmath.NewIntFromUint64(amt))
+	require.NoError(t, err)
+
 	got := incentive.ExportGenesis(ctx, *k)
 	require.NotNil(t, got)
 
