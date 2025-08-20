63c5fe3d026df609f233baa97854f05427577b75
chris <chancold123@gmail.com>
2025-08-20 15:02:32 +0900

    fix: change SigGasConsumer to accepts `ethsecp256k1` as a pubkey (#1523)
    # Description
    
    Closes: #1503
    
    ---
    
    ## Author Checklist
    
    **All** items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.
    
    I have...
    
    - [x] tackled an existing issue or discussed with a team member
    - [ ] left instructions on how to review the changes
    - [x] targeted the `main` branch
    
    ## Reviewers Checklist
    
    **All** items are required.
    Please add a note if the item is not applicable
    and please add your handle next to the items reviewed
    if you only reviewed selected items.
    
    I have...
    
    - [ ] added a relevant changelog entry to the `Unreleased` section in
    `CHANGELOG.md`
    - [ ] confirmed all author checklist items have been addressed
    - [ ] confirmed that this PR does not change production code
    - [ ] reviewed content
    - [ ] tested instructions (if applicable)
    - [ ] confirmed all CI checks have passed
    
    (cherry picked from commit 87f5a2ebe4ab2e9afc2ce28f207410c4833ea34c)

--- app/evm_ante.go
+++ app/evm_ante.go
@@ -13,7 +13,6 @@ import (
 	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	evmante "github.com/cosmos/evm/ante"
 	cosmosevmante "github.com/cosmos/evm/ante/evm"
 	"github.com/cosmos/evm/crypto/ethsecp256k1"
 	cosmosevmtypes "github.com/cosmos/evm/types"
@@ -58,7 +57,7 @@ func SigVerificationGasConsumer(
 		return nil
 
 	case *secp256k1.PubKey:
-		// Ethereum keys
+		// Cosmos keys
 		meter.ConsumeGas(params.SigVerifyCostSecp256k1, "ante verify: secp256k1")
 		return nil
 
@@ -73,9 +72,36 @@ func SigVerificationGasConsumer(
 		if !ok {
 			return fmt.Errorf("expected %T, got, %T", &signing.MultiSignatureData{}, sig.Data)
 		}
-		return evmante.ConsumeMultisignatureVerificationGas(meter, multisignature, pubkey, params, sig.Sequence)
+		return ConsumeMultisignatureVerificationGas(meter, multisignature, pubkey, params, sig.Sequence)
 
 	default:
 		return errorsmod.Wrapf(errortypes.ErrInvalidPubKey, "unrecognized/unsupported public key type: %T", pubkey)
 	}
+}
+
+// ConsumeMultisignatureVerificationGas consumes gas from a GasMeter for verifying a multisig pubkey signature
+func ConsumeMultisignatureVerificationGas(
+	meter storetypes.GasMeter, sig *signing.MultiSignatureData, pubkey multisig.PubKey,
+	params authtypes.Params, accSeq uint64,
+) error {
+	size := sig.BitArray.Count()
+	sigIndex := 0
+
+	for i := 0; i < size; i++ {
+		if !sig.BitArray.GetIndex(i) {
+			continue
+		}
+		sigV2 := signing.SignatureV2{
+			PubKey:   pubkey.GetPubKeys()[i],
+			Data:     sig.Signatures[sigIndex],
+			Sequence: accSeq,
+		}
+		err := SigVerificationGasConsumer(meter, sigV2, params)
+		if err != nil {
+			return err
+		}
+		sigIndex++
+	}
+
+	return nil
 }
