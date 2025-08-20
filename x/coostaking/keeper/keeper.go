5aed4af2d3aa974bb2528810869ef39f0086afbe
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-08-20 17:12:11 -0300

    chore: adding basic keeper

---
+++ x/coostaking/keeper/keeper.go
@@ -0,0 +1,114 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	"cosmossdk.io/collections"
+	corestoretypes "cosmossdk.io/core/store"
+	"cosmossdk.io/log"
+	"github.com/babylonlabs-io/babylon/v4/x/coostaking/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type (
+	Keeper struct {
+		cdc          codec.BinaryCodec
+		storeService corestoretypes.KVStoreService
+
+		// the address capable of executing a MsgUpdateParams message. Typically, this
+		// should be the x/gov module account.
+		authority string
+		// name of the FeeCollector ModuleAccount
+		feeCollectorName string
+
+		// Collections structures for rewards
+
+		// historicalRewards maps (period) => historicalRewards
+		historicalRewards collections.Map[uint64, types.HistoricalRewards]
+		// currentRewards stores the current rewards information
+		currentRewards collections.Item[types.CurrentRewards]
+
+		// coostakerRewardsTracker maps (CoostakerAddr) => coostakerRewardsTracker
+		coostakerRewardsTracker collections.Map[[]byte, types.CoostakerRewardsTracker]
+
+		// rewardTrackerEvents maps (babylon block height) => types.EventsPowerUpdateAtHeight
+		rewardTrackerEvents collections.Map[uint64, types.EventsPowerUpdateAtHeight]
+		// rewardTrackerEventsLastProcessedHeight keeps track of the latest processed babylon block height of events
+		rewardTrackerEventsLastProcessedHeight collections.Item[uint64]
+	}
+)
+
+func NewKeeper(
+	cdc codec.BinaryCodec,
+	storeService corestoretypes.KVStoreService,
+	bankKeeper types.BankKeeper,
+	accountKeeper types.AccountKeeper,
+	epochingKeeper types.EpochingKeeper,
+	authority string,
+	feeCollectorName string,
+) Keeper {
+	sb := collections.NewSchemaBuilder(storeService)
+
+	return Keeper{
+		cdc:            cdc,
+		storeService:   storeService,
+		bankKeeper:     bankKeeper,
+		accountKeeper:  accountKeeper,
+		epochingKeeper: epochingKeeper,
+		RefundableMsgKeySet: collections.NewKeySet(
+			sb,
+			types.RefundableMsgKeySetPrefix,
+			"refundable_msg_key_set",
+			collections.BytesKey,
+		),
+
+		// Collections structures for rewards
+		coostakerRewardsTracker: collections.NewMap(
+			sb,
+			types.BTCDelegationRewardsTrackerKeyPrefix,
+			"btc_delegation_rewards_tracker",
+			// keys: (FpAddr, DelAddr)
+			collections.PairKeyCodec(collections.BytesKey, collections.BytesKey),
+			codec.CollValue[types.BTCDelegationRewardsTracker](cdc),
+		),
+		historicalRewards: collections.NewMap(
+			sb,
+			types.FinalityProviderHistoricalRewardsKeyPrefix,
+			"fp_historical_rewards",
+			// keys: (FpAddr, period)
+			collections.PairKeyCodec(collections.BytesKey, collections.Uint64Key),
+			codec.CollValue[types.FinalityProviderHistoricalRewards](cdc),
+		),
+		currentRewards: collections.NewMap(
+			sb,
+			types.FinalityProviderCurrentRewardsKeyPrefix,
+			"fp_current_rewards",
+			// key: (FpAddr)
+			collections.BytesKey,
+			codec.CollValue[types.FinalityProviderCurrentRewards](cdc),
+		),
+		rewardTrackerEvents: collections.NewMap(
+			sb,
+			types.RewardTrackerEvents,
+			"events_reward_tracker",
+			// key: (babylon block height)
+			collections.Uint64Key,
+			codec.CollValue[types.EventsPowerUpdateAtHeight](cdc),
+		),
+		rewardTrackerEventsLastProcessedHeight: collections.NewItem[uint64](
+			sb,
+			types.RewardTrackerEventsLastProcessedHeight,
+			"last_processed_height_events_reward_tracker",
+			collections.Uint64Value,
+		),
+		authority:        authority,
+		feeCollectorName: feeCollectorName,
+	}
+}
+
+func (k Keeper) Logger(goCtx context.Context) log.Logger {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
