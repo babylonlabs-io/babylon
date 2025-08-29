f0459f7c0cc1d0ae9431b594fd97052f8e9129bb
samricotta <37125168+samricotta@users.noreply.github.com>
2025-08-29 15:00:18 +0300

    chore(zc): query APIs, exposing non-BTC-timestamped information (#1549)
    closes: https://github.com/babylonlabs-io/babylon/issues/1542
    
    Add query APIs for exposing non-BTC-timestamped information:
    
    - `LatestEpochHeader` - Get last epoch header of a given consumer ID
    - `BSNLastSentSegment` - Get last sent segment of a given consumer ID
    - `GetSealedEpochProof` - Get sealed epoch proof of a given epoch
    -prefixed with `Get` due to naming conflict with existing keeper field)
    - tests for all
    
    (cherry picked from commit 0f98ecc02da57f76c6c1d716fd868345e8777b41)
    
    # Conflicts:
    #	x/zoneconcierge/types/query.pb.go

--- x/zoneconcierge/client/cli/query.go
+++ x/zoneconcierge/client/cli/query.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
@@ -23,6 +24,10 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdFinalizedBSNsInfo())
+	cmd.AddCommand(CmdLatestEpochHeader())
+	cmd.AddCommand(CmdBSNLastSentSegment())
+	cmd.AddCommand(CmdQueryGetSealedEpochProof())
+
 	return cmd
 }
 
@@ -47,6 +52,78 @@ func CmdFinalizedBSNsInfo() *cobra.Command {
 	}
 
 	cmd.Flags().Bool("prove", false, "whether to retrieve proofs for each FinalizedBSNData")
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdLatestEpochHeader() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "latest-epoch-header <bsn-id>",
+		Short: "retrieve the latest epoch header for a given bsn",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			queryClient := types.NewQueryClient(clientCtx)
+			req := types.QueryLatestEpochHeaderRequest{ConsumerId: args[0]}
+			resp, err := queryClient.LatestEpochHeader(cmd.Context(), &req)
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(resp)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdBSNLastSentSegment() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "bsn-last-sent-seg <bsn-id>",
+		Short: "retrieve the last sent segment of a given bsn",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			queryClient := types.NewQueryClient(clientCtx)
+			req := types.QueryBSNLastSentSegmentRequest{ConsumerId: args[0]}
+			resp, err := queryClient.BSNLastSentSegment(cmd.Context(), &req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(resp)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryGetSealedEpochProof() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get-sealed-epoch-proof <epoch-num>",
+		Short: "retrieve the sealed epoch proof of a given epoch",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			queryClient := types.NewQueryClient(clientCtx)
+			epoch, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+			req := types.QueryGetSealedEpochProofRequest{EpochNum: epoch}
+			resp, err := queryClient.GetSealedEpochProof(cmd.Context(), &req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(resp)
+		},
+	}
+
 	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
