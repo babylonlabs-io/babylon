a00d3aa68eb7ae5b9c4d38a3c1851c4031254d5f
tom <tomasguerraalda@hotmail.com>
2025-09-03 12:09:18 -0300

    chore(coostaking): add init/export gen

--- x/coostaking/keeper/genesis.go
+++ x/coostaking/keeper/genesis.go
@@ -37,3 +37,109 @@ func (k Keeper) InitGenesis(ctx context.Context, gs types.GenesisState) error {
 
 	return k.SetParams(ctx, gs.Params)
 }
+
+// ExportGenesis returns the keeper state into a exported genesis state.
+func (k Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error) {
+	historicalRewards, err := k.getHistoricalRewardsEntries(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	coostakersRewardsTracker, err := k.getCoostakerRewardsTrackerEntries(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.GenesisState{
+		Params:                   k.GetParams(ctx),
+		CurrentRewards:           k.getCurrentRewardsEntry(ctx),
+		HistoricalRewards:        historicalRewards,
+		CoostakersRewardsTracker: coostakersRewardsTracker,
+	}, nil
+}
+
+// getCurrentRewardsEntry gets current rewards as genesis entry.
+func (k Keeper) getCurrentRewardsEntry(ctx context.Context) types.CurrentRewardsEntry {
+	currentRewards, err := k.GetCurrentRewards(ctx)
+	if err != nil {
+		// If no current rewards are found, return empty entry
+		return types.CurrentRewardsEntry{}
+	}
+
+	return types.CurrentRewardsEntry{Rewards: currentRewards}
+}
+
+// getHistoricalRewardsEntries gets all historical rewards stored.
+// This function has high resource consumption and should only be used on export genesis.
+func (k Keeper) getHistoricalRewardsEntries(ctx context.Context) ([]types.HistoricalRewardsEntry, error) {
+	entries := make([]types.HistoricalRewardsEntry, 0)
+
+	iter, err := k.historicalRewards.Iterate(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer iter.Close()
+
+	for ; iter.Valid(); iter.Next() {
+		period, err := iter.Key()
+		if err != nil {
+			return nil, err
+		}
+
+		rewards, err := iter.Value()
+		if err != nil {
+			return nil, err
+		}
+
+		entry := types.HistoricalRewardsEntry{
+			Period:  period,
+			Rewards: &rewards,
+		}
+
+		if err := entry.Validate(); err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, entry)
+	}
+
+	return entries, nil
+}
+
+// getCoostakerRewardsTrackerEntries gets all coostaker rewards trackers stored.
+// This function has high resource consumption and should only be used on export genesis.
+func (k Keeper) getCoostakerRewardsTrackerEntries(ctx context.Context) ([]types.CoostakerRewardsTrackerEntry, error) {
+	entries := make([]types.CoostakerRewardsTrackerEntry, 0)
+
+	iter, err := k.coostakerRewardsTracker.Iterate(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer iter.Close()
+
+	for ; iter.Valid(); iter.Next() {
+		key, err := iter.Key()
+		if err != nil {
+			return nil, err
+		}
+
+		tracker, err := iter.Value()
+		if err != nil {
+			return nil, err
+		}
+
+		addr := sdk.AccAddress(key)
+		entry := types.CoostakerRewardsTrackerEntry{
+			CoostakerAddress: addr.String(),
+			Tracker:          &tracker,
+		}
+
+		if err := entry.Validate(); err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, entry)
+	}
+
+	return entries, nil
+}
--- x/coostaking/types/genesis.go
+++ x/coostaking/types/genesis.go
@@ -1,5 +1,14 @@
 package types
 
+import (
+	"errors"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/babylonlabs-io/babylon/v4/types"
+)
+
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
@@ -11,5 +20,59 @@ func DefaultGenesis() *GenesisState {
 }
 
 func (gs GenesisState) Validate() error {
+	if err := gs.CurrentRewards.Validate(); err != nil {
+		return fmt.Errorf("invalid current rewards: %w", err)
+	}
+
+	if err := types.ValidateEntries(gs.HistoricalRewards, func(e HistoricalRewardsEntry) uint64 {
+		return e.Period
+	}); err != nil {
+		return fmt.Errorf("invalid historical rewards: %w", err)
+	}
+
+	if err := types.ValidateEntries(gs.CoostakersRewardsTracker, func(e CoostakerRewardsTrackerEntry) string {
+		return e.CoostakerAddress
+	}); err != nil {
+		return fmt.Errorf("invalid coostakers rewards tracker: %w", err)
+	}
+
+	return gs.Params.Validate()
+}
+
+// Validate validates the CurrentRewardsEntry
+func (cre CurrentRewardsEntry) Validate() error {
+	if cre.Rewards == nil {
+		return nil // empty current rewards is valid
+	}
+	return cre.Rewards.Validate()
+}
+
+// Validate validates the HistoricalRewardsEntry
+func (hre HistoricalRewardsEntry) Validate() error {
+	if hre.Rewards == nil {
+		return fmt.Errorf("historical rewards at period %d is nil", hre.Period)
+	}
+	return hre.Rewards.Validate()
+}
+
+// Validate validates the CoostakerRewardsTrackerEntry
+func (crte CoostakerRewardsTrackerEntry) Validate() error {
+	if err := validateAddrStr(crte.CoostakerAddress); err != nil {
+		return fmt.Errorf("invalid coostaker address: %w", err)
+	}
+	if crte.Tracker == nil {
+		return fmt.Errorf("tracker for coostaker address %s is nil", crte.CoostakerAddress)
+	}
+	return crte.Tracker.Validate()
+}
+
+// validateAddrStr validates an address string
+func validateAddrStr(addr string) error {
+	if addr == "" {
+		return errors.New("empty address")
+	}
+	if _, err := sdk.AccAddressFromBech32(addr); err != nil {
+		return fmt.Errorf("invalid address: %s, error: %w", addr, err)
+	}
 	return nil
 }
