ee216e63ceb4755235c97e41c030228c3977cd6d
jhBharvest <jeonghyuk@bharvest.io>
2025-09-17 04:50:02 +0900

    Add : Upgrade handler for epoching spam prevention (#1663) (#1703)
    # Description
    
    Closes: #1662
    
    Add upgrade handler logic to enable epoching spam prevention.
    
    **Testnet:** upgrade handler applied in v3rc4
    
    **Mainnet:** upgrade handler applied in v3
    
    ## Notes
    **Upgrade must occur at the first block of the epoch; if the block
    number is incorrect, the upgrade fails.**
    
    - Add v3rc4/upgrades.go: upgrade logic for testnet.
    - Add v3rc4/upgrades_test.go: upgrade test code.
    - Add validation.go: common validation logic for upgrade.
    - Modify v3/upgrades.go: add validation logic for epoching spam
    prevention upgrade.
    - Modify v3/upgrades_test.go: fix dummyUpgradeHeight to first block of
    epoch, add validation for epoching.
    - Modify e2e/configurer/upgrade.go: fix ChainConfig.UpgradeHeight to
    first block of epoch.
    
    ## Progress check list
    
    - [x] Implementation upgrade handler for v3rc4
    - [x] Implementation upgrade handler for v3
    
    ---------
    
    Co-authored-by: jhlee <jhlee@jhleeui-MacBookAir.local>
    (cherry picked from commit 5cbf5d534774e1d29008d88c85042d04a0a67b7c)
    
    # Conflicts:
    #	CHANGELOG.md
    #	app/include_upgrade_testnet.go
    #	app/upgrades/v3/upgrades.go
    #	app/upgrades/v3/upgrades_test.go

--- app/include_upgrade_testnet.go
+++ app/include_upgrade_testnet.go
@@ -3,6 +3,7 @@
 package app
 
 import (
+<<<<<<< HEAD
 	"github.com/babylonlabs-io/babylon/v3/app/upgrades"
 	v2 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v2"
 	v22 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v2_2"
@@ -10,12 +11,23 @@ import (
 	v3 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v3"
 	v3rc2 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v3rc2/testnet"
 	v3rc3 "github.com/babylonlabs-io/babylon/v3/app/upgrades/v3rc3"
+=======
+	"github.com/babylonlabs-io/babylon/v4/app/upgrades"
+	v2 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2"
+	v22 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2_2"
+	v2rc4 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2rc4/testnet"
+	v3 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v3"
+	v3rc2 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v3rc2/testnet"
+	v3rc3 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v3rc3"
+	v3rc4 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v3rc4/testnet"
+>>>>>>> 5cbf5d53 (Add : Upgrade handler for epoching spam prevention (#1663) (#1703))
 )
 
 // init is used to include v1 upgrade testnet data
 // it is also used for e2e testing
 func init() {
 	Upgrades = []upgrades.Upgrade{
+		v3rc4.Upgrade,
 		v3rc3.Upgrade, // same as v2_3 mainnet
 		v3rc2.Upgrade,
 		v3.CreateUpgrade(false, 10, 264773, 2419200), // TODO: to be updated
