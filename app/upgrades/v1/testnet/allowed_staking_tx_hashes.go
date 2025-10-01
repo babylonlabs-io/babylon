603e1d8e75f3aeaa54a974a5ef4fea1cc1eb66e3
maiquanghiep <maiquanghiepdl@gmail.com>
2025-10-01 21:17:29 +0700

    Update allowed_staking_tx_hashes.go

--- app/upgrades/v1/testnet/allowed_staking_tx_hashes.go
+++ app/upgrades/v1/testnet/allowed_staking_tx_hashes.go
@@ -3,5 +3,8 @@ package testnet
 // Specifies all the cap1 testnet-4 allowed staking tx hashes to migrate
 // to BTC delegation
 const AllowedStakingTxHashesStr = `{
-  "tx_hashes": []
+  "tx_hashes": [
+      "5748f9a245ec52e04a312ec0433da7ecbb769af0a1dc6ae26ad34ed151a0e526",
+      "023c4d0fc8eeb2c90c65b533996fbc645278c7545900943094cb120e05061bfd"
+  ]
 }`
