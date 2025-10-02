16868c4beebef1346a874a271f74c8fdc196551c
maiquanghiep <maiquanghiepdl@gmail.com>
2025-10-02 11:16:51 +0700

    Update token_distribution.go

--- app/upgrades/v1/testnet/token_distribution.go
+++ app/upgrades/v1/testnet/token_distribution.go
@@ -2,16 +2,5 @@ package testnet
 
 // Simulates 50k wallets receiving funds between 1000000ubbn ~ 3000000ubbn
 const TokensDistributionStr = `{
-  "token_distribution": [
-    {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
-      "address_receiver": "bbn1ut28z83nyfq9ntnys0yd7sq367w9q7fmet0zsj",
-      "amount": 1018474
-    },
-    {
-      "address_sender": "bbn1w9cfa0qzlx8ktdecnctpxe0en0ct2985v0c42g",
-      "address_receiver": "bbn13sp6kc680l2vwd48fxw5mjnfthy663lmlpuest",
-      "amount": 1021969
-    }
-  ]
+  "token_distribution": []
 }`
