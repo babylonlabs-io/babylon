39d98894403d735ac0db9a94724c88c02ad36ed0
KonradStaniec <konrad.staniec@gmail.com>
2025-10-31 09:47:33 +0100

    v4rc3 upgrade handler (#1848)
    Next testnet release will be breaking so it needs proper upgrade handler

--- app/include_upgrade_testnet.go
+++ app/include_upgrade_testnet.go
@@ -13,12 +13,14 @@ import (
 	v23 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2_3"
 	v2rc4 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v2rc4/testnet"
 	v4 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v4"
+	v4rc3 "github.com/babylonlabs-io/babylon/v4/app/upgrades/v4rc3/testnet"
 )
 
 // init is used to include v1 upgrade testnet data
 // it is also used for e2e testing
 func init() {
 	Upgrades = []upgrades.Upgrade{
+		v4rc3.Upgrade,
 		v4.Upgrade,
 		v23.Upgrade,
 		v22.Upgrade,
