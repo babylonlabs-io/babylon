6e8bdd328a47343fcd7ad98d1b0c7267860b019a
Runchao Han <me@runchao.rocks>
2025-11-24 16:11:51 +0800

    Merge commit from fork
    * init
    
    * pr comments
    
    * simplify address type parsing
    
    * changelog
    
    * compare sighash types rather than raw bytes
    
    * follow camel case for naming
    
    * add vanilla upgrade handler for v4.1
    
    * add v4.1 upgrade to mainnet
    
    * cl

---
+++ app/upgrades/v4_1/upgrade.go
@@ -0,0 +1,34 @@
+package testnet
+
+import (
+	"context"
+
+	store "cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/babylonlabs-io/babylon/v4/app/keepers"
+	"github.com/babylonlabs-io/babylon/v4/app/upgrades"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+const UpgradeName = "v4.1"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
+
+func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, keepers *keepers.AppKeepers) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// Run migrations before applying any other state changes.
+		migrations, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return nil, err
+		}
+
+		return migrations, nil
+	}
+}
