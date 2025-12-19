2be8b933756a8a60e7713d7ff46f95ab6038a375
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-12-19 13:29:57 -0300

    chore: tryfix e2e rwd btc

--- test/e2e/btc_rewards_distribution_e2e_test.go
+++ test/e2e/btc_rewards_distribution_e2e_test.go
@@ -392,7 +392,7 @@ func (s *BtcRewardsDistribution) CheckRewardsFirstDelegations() {
 
 	coins.RequireCoinsDiffInPointOnePercentMargin(
 		s.T(),
-		fp2DiffRewards.Coins.MulInt(sdkmath.NewIntFromUint64(3)).Add(sdk.NewCoin("ubbn", sdkmath.NewInt(2))), // truncation rounding
+		fp2DiffRewards.Coins.MulInt(sdkmath.NewIntFromUint64(3)), // truncation rounding
 		fp1DiffRewards.Coins,
 	)
 
